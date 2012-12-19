b35d9b812f0c63cae42e1bab12f2f04760036fa4
zyizhang <zyizhang@c3b84367-c167-4d52-9215-bf13455bd1c0>
2012-12-19 13:23:38 +0000

    remove un-necessary files.
    git-svn-id: file:///Users/zhangyi/Documents/svnRepository/staging/staging/trunk@20 c3b84367-c167-4d52-9215-bf13455bd1c0

--- moveMainframe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/staging/v2 
-/opt/staging/v2/moveMainframe.pl
--- moveSwkbtFiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mv /var/ftp/swkbt/* /var/http_reports/SwKb/
-chown www.www /var/http_reports/SwKb/*
-
-find /var/http_reports/SwKb/* -mtime +14 -exec rm {} \;
