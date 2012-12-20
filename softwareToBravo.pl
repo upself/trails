27a60c369d3fed20eea7c1098d5f09320e56279d
zyizhang <zyizhang@c3b84367-c167-4d52-9215-bf13455bd1c0>
2012-12-20 12:19:18 +0000

    code merge: merge the softwareToBravo code from tap3.
    git-svn-id: file:///Users/zhangyi/Documents/svnRepository/staging/trunk@27 c3b84367-c167-4d52-9215-bf13455bd1c0

--- softwareToBravo.pl
+++ softwareToBravo.pl
@@ -6,230 +6,450 @@ use File::Copy;
 use File::Basename;
 use Base::Utils;
 use Sigbank::Delegate::SystemScheduleStatusDelegate;
-use Staging::Delegate::StagingDelegate;
+use Database::Connection;
 use Base::ConfigManager;
-use Tap::NewPerl; 
+use BRAVO::SoftwareLoader;
+use Tap::NewPerl;
 
-###Globals
-my $maxLparsInQuery = 1000;
-my $logfile         = "/var/staging/logs/softwareToBravo/softwareToBravo.log";
-my $pidFile         = "/tmp/softwareToBravo.pid";
-my $configFile          = "/opt/staging/v2/config/softwareToBravoConfig.txt";
+my $logfile    = "/var/staging/logs/softwareToBravo/softwareToBravo.log";
+my $pidFile    = "/tmp/softwareToBravo.pid";
+my $configFile = "/opt/staging/v2/config/softwareToBravoConfig.txt";
 
-###Initialize properties
-my $cfgMgr =  Base::ConfigManager->instance($configFile);
-my $sleepPeriod         = $cfgMgr->sleepPeriod;
-my $server              = $cfgMgr->server;
-my $testMode            = $cfgMgr->testMode;
-my $loadDeltaOnly       = $cfgMgr->loadDeltaOnly;
-my $applyChanges        = $cfgMgr->applyChanges;
+my $cfgMgr       = Base::ConfigManager->instance($configFile);
+my $sleepPeriod  = $cfgMgr->sleepPeriod;
+my $server       = $cfgMgr->server;
+my $testMode     = $cfgMgr->testMode;
+my $applyChanges = $cfgMgr->applyChanges;
 
 ###Validate server
 die "!!! ONLY RUN THIS LOADER ON $server !!!\n"
-  unless validateServer($server);
+    unless validateServer($server);
 
-###Make a daemon.
-umask 0;
-defined( my $pid = fork )
-  or die "ERROR: Unable to fork: $!";
-exit if $pid;
-setsid or die "ERROR: Unable to setsid: $!";
-
-####Handle usage and user action.
-loaderStart( shift @ARGV, $pidFile );
-
-###Close handles to avoid console output.
-open( STDIN, "/dev/null" )
-  or die "ERROR: Unable to direct STDIN to /dev/null: $!";
-open( STDOUT, "/dev/null" )
-  or die "ERROR: Unable to direct STDOUT to /dev/null: $!";
-open( STDERR, "/dev/null" )
-  or die "ERROR: Unable to direct STDERR to /dev/null: $!";
-
-###Set the logging level
-logging_level($cfgMgr->debugLevel);
-
-###Set the logfile
+logging_level( $cfgMgr->debugLevel );
 logfile($logfile);
 
-###Setup for forking children.
-my $maxChildren = 20;
-my %children    = ();
-my $children    = 0;
-my $sleepTime   = 60;
-my $childScript =
-  ( dirname $0) . "/" . ( split( /\./, ( basename $0) ) )[0] . "Child.pl";
+my $job                  = 'STAGING TO BRAVO';
+my $systemScheduleStatus = startJob($job);
 
-###Setup logArray
-my @logArray = ();
-for my $i ( 1 .. $maxChildren ) {
-	push @logArray, $i;
+my $maxChildren        = 50;
+my %runningCustomerIds = ();
+my %children           = ();
+my $children;
+
+my $connection = Database::Connection->new('staging');
+my @customerIds = getStagingQueue( $connection, 0 );
+$connection->disconnect;
+
+daemonize();
+spawnChildren();
+keepTicking();
+endJob($systemScheduleStatus);
+exit;
+
+sub spawnChildren {
+    wlog("Spawning children");
+    for ( my $i = 0; $i < $maxChildren; $i++ ) {
+        my $customer = shift @customerIds;
+        my ( $date, $customerId ) = each %$customer;
+        if ( isCustomerRunning( $customerId, $date ) == 1 ) {
+            next;
+        }
+        newChild( $customerId, $date, 0 );
+        if ( scalar @customerIds == 0 ) {
+            last;
+        }
+    }
 }
 
-###Signal handler for dead children
-sub REAPER {
-	$SIG{CHLD} = \&REAPER;
-	while ( ( my $pid = waitpid( -1, &WNOHANG ) ) > 0 ) {
-		if ( exists( $children{$pid} ) ) {
-			$children--;
-			push @logArray, $children{$pid}{'log'};
-			delete $children{$pid};
-		}
-		else {
-			wlog("i should not hit this in the reaper sub");
-		}
-	}
-}
-$SIG{CHLD} = \&REAPER;
-
-###Set the job name of this script to update the status
-my $job;
-if ( $loadDeltaOnly == 1 ) {
-	$job = 'STAGING TO BRAVO - SW (DELTA)';
-}
-else {
-	$job = 'STAGING TO BRAVO - SW (FULL)';
-}
-dlog("job=$job");
-my $systemScheduleStatus;
-
-###Wrap everything in an eval so we can capture in logfile.
-eval {
-	###Execute loader once, and then continue to execute
-	###based on existance of pid file.
-	$| = 1;
-        my $count = 0;
-	while (1) {
-                if($count > 5) {
+sub keepTicking {
+    wlog("Keep on ticking");
+    my $count = 0;
+    while (1) {
+        if ( scalar @customerIds == 0 ) {
+             my $connection = Database::Connection->new('staging');
+             @customerIds = getStagingQueue( $connection, $count );
+             $connection->disconnect;
+        }
+        if ( $children >= $maxChildren ) {
+            wlog("sleeping");
+            sleep;
+            wlog("done sleeping");
+        }
+        for ( my $i = $children; $i < $maxChildren; $i++ ) {
+            wlog("running $i");
+            my $customer = shift @customerIds;
+            my ( $date, $customerId ) = each %$customer;
+            if ( isCustomerRunning( $customerId, $date ) == 1 ) {
+                $i--;
+                next;
+            }
+            newChild( $customerId, $date, $count );
+            if ( scalar @customerIds == 0 ) {
+                sleep 5;
+                $count++;
+                if ( $count > 5 ) {
                     $count = 0;
                 }
-
-		###Notify system schedule status that we are starting.
-		dlog("job=$job");
-		if ( $applyChanges == 1 ) {
-			###Notify the scheduler that we are starting
-			ilog("starting $job system schedule status");
-			$systemScheduleStatus = SystemScheduleStatusDelegate->start($job);
-			ilog("started $job system schedule status");
-		}
-
-		###Get a connection to staging
-		ilog("getting staging db connection");
-		my $stagingConnection = Database::Connection->new('staging');
-		die "Unable to get staging db connection!\n"
-		  unless defined $stagingConnection;
-		ilog("got staging db connection");
-
-		###Get batches from staging to process.
-		my @batches;
-		eval {
-			###Get the current software lpar batches to process.
-			@batches = Staging::Delegate::StagingDelegate->getSoftwareLparBatches(
-				$stagingConnection, $testMode,
-				$loadDeltaOnly,     $maxLparsInQuery, $count
-			);
-		};
-		if ($@) {
-			###Close the staging db connection
-			ilog("disconnecting staging db connection");
-			$stagingConnection->disconnect;
-			ilog("disconnected staging db connection");
-			die $@;
-		}
-
-		###Close the staging db connection
-		ilog("disconnecting staging db connection");
-		$stagingConnection->disconnect;
-		ilog("disconnected staging db connection");
-
-		###Process each batch with max children forks.
-		foreach my $batch (@batches) {
-			my $firstId = ( split( /\,/, $batch ) )[0];
-			my $lastId  = ( split( /\,/, $batch ) )[1];
-			ilog("batch: firstId=$firstId, lastId=$lastId");
-
-			###Spawn child unless maxed out.
-			while ( $children >= $maxChildren ) {
-
-				#ilog("sleeping");
-				sleep $sleepTime;
-			}
-
-			my $logNum   = shift @logArray;
-			my $childLog = $logfile . ".child." . $logNum;
-			spawnScript( $firstId, $lastId, $childLog, $logNum );
-                        sleep 5;
-		}
-		ilog("completed looping over batches");
-
-		###Wait till  all children die.
-		while ( $children != 0 ) {
-			ilog("waiting for children to stop, count: $children");
-			sleep 5;
-		}
-		ilog("all children are dead");
-
-		###Check if we should stop.
-		if ( loaderCheckForStop($pidFile) == 1 ) {
-			ilog("stopping daemon");
-			last;
-		}
-		sleep $sleepPeriod;
-                $count++;
-	}
-};
-if ($@) {
-	elog($@);
-	die $@;
-
-	if ( $applyChanges == 1 ) {
-
-		###Notify the scheduler that we had an error
-		ilog("erroring $job system schedule status");
-		SystemScheduleStatusDelegate->error($systemScheduleStatus);
-		ilog("errored $job system schedule status");
-	}
-}
-else {
-
-	if ( $applyChanges == 1 ) {
-
-		###Notify the scheduler that we are stopping
-		ilog("stopping $job system schedule status");
-		SystemScheduleStatusDelegate->stop($systemScheduleStatus);
-		ilog("stopped $job system schedule status");
-	}
+                last;
+            }
+        }
+    }
 }
 
-exit 0;
-
-sub spawnScript {
-	my $firstId  = shift;
-	my $lastId   = shift;
-	my $childLog = shift;
-	my $logNum   = shift;
-	my $pid;
-
-	unless ( defined( $pid = fork ) ) {
-		elog("ERROR: unable to fork child process!");
-		exit 1;
-	}
-
-	if ($pid) {
-		###I am the parent
-		$children{$pid}{'log'} = $logNum;
-		$children++;
-		return;
-	}
-	else {
-		###I am the child, i *CANNOT* return only exit
-		my $cmd =
-		    "$childScript"
-		  . " -t $testMode -d $loadDeltaOnly -a $applyChanges -m $maxLparsInQuery"
-		  . " -b $firstId -e $lastId -l $childLog -c $configFile";
-		ilog("spawning child: $cmd");
-		`$cmd >>$childLog 2>&1`;
-		ilog("child complete: $cmd");
-		exit 0;
-	}
+sub isCustomerRunning {
+    my $customerId = shift;
+    my $date       = shift;
+    my $result     = 0;
+    wlog("Checking $customerId, $date");
+    if ( exists $runningCustomerIds{$customerId}{$date} ) {
+        wlog("$customerId, $date is running");
+        $result = 1;
+    }
+    return $result;
 }
+
+sub newChild {
+    my $customerId = shift;
+    my $date       = shift;
+    my $phase      = shift;
+    my $pid;
+
+    wlog("spawning $customerId, $date, $phase");
+    my $sigset = POSIX::SigSet->new(SIGINT);
+    sigprocmask( SIG_BLOCK, $sigset ) or die "Can't block SIGINT for fork: $!";
+    die "Cannot fork child: $!\n" unless defined( $pid = fork );
+    if ($pid) {
+        $children{$pid} = 1;
+        $children++;
+        $runningCustomerIds{$customerId}{$date} = $pid;
+        ilog("forked new child, we now have $children children");
+        return;
+    }
+
+    my $stagingConnection = Database::Connection->new('staging');
+    my $trailsConnection  = Database::Connection->new('trails');
+    my $swassetConnection = Database::Connection->new('swasset');
+
+    my @lpars = findSoftwareLparsByCustomerIdByDate( $customerId, $date, $phase, $stagingConnection );
+    wlog( "Child $customerId, $date, " . scalar @lpars . " running -- $phase" );
+    foreach my $id (@lpars) {
+        my $loader = new BRAVO::SoftwareLoader( $stagingConnection, $trailsConnection, $swassetConnection );
+        $loader->load(
+                       LoadDeltaOnly => 1,
+                       ApplyChanges  => 1,
+                       CustomerId    => $customerId,
+                       LparId        => $id,
+                       Phase         => 1
+        );
+    }
+
+    $stagingConnection->disconnect;
+    $trailsConnection->disconnect;
+    $swassetConnection->disconnect;
+    wlog( "Child $customerId, $date," . scalar @lpars . " done -- $phase" );
+    exit;
+}
+
+sub REAPER {
+    my $stiff;
+    while ( ( $stiff = waitpid( -1, &WNOHANG ) ) > 0 ) {
+        warn("child $stiff terminated -- status $?");
+        $children--;
+        foreach my $customerId ( keys %runningCustomerIds ) {
+            foreach my $date ( keys %{ $runningCustomerIds{$customerId} } ) {
+                if ( $stiff == $runningCustomerIds{$customerId}{$date} ) {
+                    delete $runningCustomerIds{$customerId}{$date};
+                }
+            }
+        }
+    }
+    $SIG{CHLD} = \&REAPER;
+}
+
+sub daemonize {
+    my $pid = fork;
+    defined($pid) or die "Cannot start daemon: $!";
+    print "Parent daemon running.\n" if $pid;
+    exit if $pid;
+
+    POSIX::setsid();
+    loaderStart( shift @ARGV, $pidFile );
+
+    close(STDOUT);
+    close(STDIN);
+    close(STDERR);
+
+    $SIG{__WARN__} = sub {
+        ilog( "NOTE! " . join( " ", @_ ) );
+    };
+
+    $SIG{__DIE__} = sub {
+        elog( "FATAL! " . join( " ", @_ ) );
+        exit;
+    };
+
+    $SIG{HUP} = $SIG{INT} = $SIG{TERM} = sub {
+        my $sig = shift;
+        $SIG{$sig} = 'IGNORE';
+        kill 'INT' => keys %children;
+        die "killed by $sig\n";
+        exit;
+    };
+
+    $SIG{CHLD} = \&REAPER;
+}
+
+sub startJob {
+    my $job = shift;
+    return SystemScheduleStatusDelegate->start($job);
+}
+
+sub endJob {
+    my $systemScheduleStatus = shift;
+    SystemScheduleStatusDelegate->stop($systemScheduleStatus);
+}
+
+sub getStagingQueue {
+    my ( $connection, $count ) = @_;
+
+    my @customers;
+
+    ###Prepare query to pull software lpar ids from staging
+    dlog("preparing software lpar ids query");
+    $connection->prepareSqlQueryAndFields( querySoftwareLparCustomers($count) );
+    dlog("prepared software lpar ids query");
+
+    ###Get the statement handle
+    dlog("getting sth for software lpar ids query");
+    my $sth = $connection->sql->{softwareLparCustomers};
+    dlog("got sth for software lpar ids query");
+
+    ###Bind our columns
+    my %rec;
+    dlog("binding columns for software lpar ids query");
+    $sth->bind_columns( map { \$rec{$_} } @{ $connection->sql->{softwareLparCustomersFields} } );
+    dlog("binded columns for software lpar ids query");
+
+    ###Excute the query
+    ilog("executing software lpar ids query");
+    $sth->execute();
+    ilog("executed software lpar ids query");
+
+    while ( $sth->fetchrow_arrayref ) {
+        cleanValues( \%rec );
+
+        my %data;
+        $data{ $rec{date} } = $rec{customerId};
+        push @customers, \%data;
+    }
+
+    return @customers;
+}
+
+sub querySoftwareLparCustomers {
+    my ($count) = @_;
+    my @fields  = (qw( customerId date ));
+    my $query   = '
+        select
+            a.customer_id
+            ,date(a.scan_time)
+        from software_lpar a
+        left outer join software_lpar_map b on
+            a.id = b.software_lpar_id
+        left outer join scan_record c on
+            b.scan_record_id = c.id
+    ';
+    if ( $count == 1 ) {
+        $query .= '
+        left outer join software_manual sm on
+            c.id = sm.scan_record_id
+        ';
+    }
+    elsif ( $count == 2 ) {
+        $query .= '
+        left outer join software_dorana sd on
+            c.id = sd.scan_record_id
+        ';
+    }
+    elsif ( $count == 3 ) {
+        $query .= '
+        left outer join software_tlcmz st on
+            c.id = st.scan_record_id
+        ';
+    }
+    elsif ( $count == 4 ) {
+        $query .= '
+        left outer join software_filter sf on
+            c.id = sf.scan_record_id
+        ';
+    }
+    elsif ( $count == 5 ) {
+        $query .= '
+        left outer join software_signature ss on
+            c.id = ss.scan_record_id
+        ';
+    }
+    my $clause = 'where';
+    $query .= ' ' . $clause . ' ( (
+        a.action != \'COMPLETE\'
+        or b.action != \'COMPLETE\' ';
+    if ( $count == 1 ) {
+        $query .= '
+                or sm.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $count == 2 ) {
+        $query .= '
+                or sd.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $count == 3 ) {
+        $query .= '
+                or st.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $count == 4 ) {
+        $query .= '
+                or sf.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $count == 5 ) {
+        $query .= '
+                or ss.action != \'COMPLETE\'
+            ';
+    }
+    $query .= '))';
+    $clause = 'and';
+
+    $query .= '
+        group by
+            a.customer_id
+            ,date(a.scan_time)
+        order by
+            date(a.scan_time)
+        with ur
+    ';
+    dlog("querySoftwareLparCustomers=$query");
+    return ( 'softwareLparCustomers', $query, \@fields );
+}
+
+sub findSoftwareLparsByCustomerIdByDate {
+    my ( $customerId, $date, $phase, $connection ) = @_;
+
+    my @lparIds;
+
+    ###Prepare query to pull software lpar ids from staging
+    dlog("preparing software lpar ids query");
+    $connection->prepareSqlQueryAndFields( querySoftwareLparsByCustomerIdByDate($phase) );
+    dlog("prepared software lpar ids query");
+
+    ###Get the statement handle
+    dlog("getting sth for software lpar ids query");
+    my $sth = $connection->sql->{softwareLparsByCustomerIdByDate};
+    dlog("got sth for software lpar ids query");
+
+    ###Bind our columns
+    my %rec;
+    dlog("binding columns for software lpar ids query");
+    $sth->bind_columns( map { \$rec{$_} } @{ $connection->sql->{softwareLparsByCustomerIdByDateFields} } );
+    dlog("binded columns for software lpar ids query");
+
+    ###Excute the query
+    ilog("executing software lpar ids query");
+    $sth->execute( $customerId, $date );
+    ilog("executed software lpar ids query");
+
+    while ( $sth->fetchrow_arrayref ) {
+        cleanValues( \%rec );
+        push @lparIds, $rec{id};
+    }
+
+    return @lparIds;
+}
+
+sub querySoftwareLparsByCustomerIdByDate {
+    my $phase  = shift;
+    my @fields = (qw( id ));
+    my $query  = '
+        select
+            a.id
+        from software_lpar a
+        left outer join software_lpar_map b on
+            a.id = b.software_lpar_id
+        left outer join scan_record c on
+            b.scan_record_id = c.id
+    ';
+    if ( $phase == 1 ) {
+        $query .= '
+        left outer join software_manual sm on
+            c.id = sm.scan_record_id
+        ';
+    }
+    elsif ( $phase == 2 ) {
+        $query .= '
+        left outer join software_dorana sd on
+            c.id = sd.scan_record_id
+        ';
+    }
+    elsif ( $phase == 3 ) {
+        $query .= '
+        left outer join software_tlcmz st on
+            c.id = st.scan_record_id
+        ';
+    }
+    elsif ( $phase == 4 ) {
+        $query .= '
+        left outer join software_filter sf on
+            c.id = sf.scan_record_id
+        ';
+    }
+    elsif ( $phase == 5 ) {
+        $query .= '
+        left outer join software_signature ss on
+            c.id = ss.scan_record_id
+        ';
+    }
+    $query .= '
+        where
+            a.customer_id = ?
+            and date(a.scan_time) = ?
+            and (a.action != \'COMPLETE\'
+            or b.action != \'COMPLETE\'
+        ';
+    if ( $phase == 1 ) {
+        $query .= '
+                or sm.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $phase == 2 ) {
+        $query .= '
+                or sd.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $phase == 3 ) {
+        $query .= '
+                or st.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $phase == 4 ) {
+        $query .= '
+                or sf.action != \'COMPLETE\'
+            ';
+    }
+    elsif ( $phase == 5 ) {
+        $query .= '
+                or ss.action != \'COMPLETE\'
+            ';
+    }
+    $query .= ')';
+    $query .= '
+        group by
+            a.id
+        order by
+            a.id
+        with ur
+    ';
+    return ( 'softwareLparsByCustomerIdByDate', $query, \@fields );
+}
+
+
