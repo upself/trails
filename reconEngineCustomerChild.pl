644eb4c38e946465c05bec6bdb5ea2cd0c56d0c1
zyizhang <zyizhang@c3b84367-c167-4d52-9215-bf13455bd1c0>
2013-01-02 09:11:58 +0000

    merge the code from tap3 for recon main script.
    git-svn-id: file:///Users/zhangyi/Documents/svnRepository/staging/trunk@30 c3b84367-c167-4d52-9215-bf13455bd1c0

--- reconEngineCustomerChild.pl
+++ reconEngineCustomerChild.pl
@@ -6,17 +6,23 @@ use File::Copy;
 use Base::Utils;
 use Recon::ReconEngineCustomer;
 use Base::ConfigManager;
-use Tap::NewPerl; 
+use Tap::NewPerl;
 
 ###Check usage.
-use vars qw( $opt_t $opt_a $opt_c $opt_l $opt_f );
-getopts("t:a:c:l:f:");
-die "Usage: $0" . " -t <test-mode> -a <apply-changes> -c <customer-id>" . " -l <logfile> -f <configFile>\n"    
-  unless (    defined $opt_t
-           && defined $opt_a
-           && defined $opt_c
-           && defined $opt_l 
-           && defined $opt_f);
+use vars qw( $opt_t $opt_a $opt_c $opt_l $opt_f $opt_d $opt_p);
+getopts("t:a:c:l:f:d:p:");
+die "Usage: $0"
+  . " -t <test-mode> -a <apply-changes> -c <customer-id>"
+  . " -l <logfile> -f <configFile> -p <pool> -d <date> \n"
+  unless (
+    defined $opt_t
+ && defined $opt_a
+ && defined $opt_c
+ && defined $opt_l
+ && defined $opt_f 
+ && defined $opt_d 
+ && defined $opt_p
+  );
 
 ###Close handles to avoid console output.
 open( STDIN, "/dev/null" )
@@ -27,30 +33,35 @@ open( STDERR, "/dev/null" )
   or die "ERROR: Unable to direct STDERR to /dev/null: $!";
 
 eval {
-    my $cfgMgr =  Base::ConfigManager->instance($opt_f);    
+ my $cfgMgr = Base::ConfigManager->instance($opt_f);
 
-	logfile($opt_l);
-	logging_level($cfgMgr->debugLevel);
+ logfile($opt_l);
+ logging_level( $cfgMgr->debugLevel );
 
-    ilog(   "starting customer child: testMode=$opt_t"
-          . ", applyChanges=$opt_a"
-          . ", customerId=$opt_c"
-          . ", logfile=$opt_l" 
-          . ", configFile=$opt_f");
+ ilog("starting customer child: testMode=$opt_t"
+    . ", applyChanges=$opt_a"
+    . ", customerId=$opt_c"
+    . ", logfile=$opt_l"
+    . ", configFile=$opt_f"
+    . ", pooled=$opt_p"
+    . ", date=$opt_d" );
 
-    ###Call recon engine for this customer id.
-    my $reconEngine = new Recon::ReconEngineCustomer($opt_c);
-    $reconEngine->recon;
+ ###Call recon engine for this customer id.
+ my $reconEngine =
+   new Recon::ReconEngineCustomer( $customerId, $date, $poolRunning );
+ $reconEngine->recon;
 
-    ilog(   "stopping customer child: testMode=$opt_t"
-          . ", applyChanges=$opt_a"
-          . ", customerId=$opt_c"
-          . ", logfile=$opt_l" 
-          . ", configFile=$opt_f");
+ ilog("stopping customer child: testMode=$opt_t"
+    . ", applyChanges=$opt_a"
+    . ", customerId=$opt_c"
+    . ", logfile=$opt_l"
+    . ", configFile=$opt_f"
+    . ", pooled=$opt_p"
+    . ", date=$opt_d" );
 };
 if ($@) {
-    elog($@);
-    die $@;
+ elog($@);
+ die $@;
 }
 
 exit 0;
