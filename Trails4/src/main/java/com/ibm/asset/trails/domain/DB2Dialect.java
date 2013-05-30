807f6c7bba532e111d02e774166b25b4e11c99b1
zyizhang@cn.ibm.com <zyizhang@cn.ibm.com@35984d28-f107-47b3-a003-c646fc333b22>
2013-05-30 09:46:06 +0000

    take out tagged project.
    git-svn-id: file:///Users/zhangyi/Documents/svn/trails/trunk@19 35984d28-f107-47b3-a003-c646fc333b22

--- Trails4/src/main/java/com/ibm/asset/trails/domain/DB2Dialect.java
+++
@@ -1,8 +0,0 @@
-package com.ibm.asset.trails.domain;
-
-public class DB2Dialect extends org.hibernate.dialect.DB2Dialect {
-	@Override
-	public String getCrossJoinSeparator() {
-		return ", ";
-	}
-}
--- Trails4/src/main/webapp/index.jsp
+++
@@ -1,3 +0,0 @@
-<%@ taglib prefix="s" uri="/struts-tags"%>
-<s:action name="home" executeResult="true" />
-
