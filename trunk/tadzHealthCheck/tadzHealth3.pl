c84aaf28db77725fbdc8ca4e92c6d8e5ce718c32
dbryson <dbryson@506d4fb4-79cc-11e2-8167-7963c9775998>
2013-06-06 14:51:34 +0000

    this fixes the drop-out we saw where all the STAGING lpars were not being retrieved from TRAILS because the SQL query string was too large.
    git-svn-id: http://tapmf.boulder.ibm.com/svn/reports@41 506d4fb4-79cc-11e2-8167-7963c9775998

--- trunk/tadzHealthCheck/tadzHealth3.pl
+++ trunk/tadzHealthCheck/tadzHealth3.pl
@@ -4,35 +4,41 @@ use DBI;
 use Config::Properties::Simple;
 use Getopt::Std;
 
-use strict;  
+use strict;
 
-our ( $opt_u );
+our ($opt_u);
 getopts("u:");
 usage() unless ( defined $opt_u );
 
+my $cfg = Config::Properties::Simple->new( file => $opt_u . ".ini" );
 
-my $cfg = Config::Properties::Simple->new(file => $opt_u .  ".ini"); 
-
-my $stageDatabase = $cfg->getProperty('stageDatabase');
-my $stageDatabaseUser = $cfg->getProperty('stageDatabaseUser');
+my $stageDatabase         = $cfg->getProperty('stageDatabase');
+my $stageDatabaseUser     = $cfg->getProperty('stageDatabaseUser');
 my $stageDatabasePassword = $cfg->getProperty('stageDatabasePassword');
 
-my $reportDatabase = $cfg->getProperty('reportDatabase');
-my $reportDatabaseUser = $cfg->getProperty('reportDatabaseUser');
+my $reportDatabase         = $cfg->getProperty('reportDatabase');
+my $reportDatabaseUser     = $cfg->getProperty('reportDatabaseUser');
 my $reportDatabasePassword = $cfg->getProperty('reportDatabasePassword');
 
-# Open a connections 
-my $dbhReporting = DBI->connect("dbi:DB2:$reportDatabase", "$reportDatabaseUser", "$reportDatabasePassword", {RaiseError => 1});
-my $dbhStage = DBI->connect("dbi:DB2:$stageDatabase", "$stageDatabaseUser", "$stageDatabasePassword", {RaiseError => 1});
+# Open a connections
+my $dbhReporting = DBI->connect(
+	"dbi:DB2:$reportDatabase", "$reportDatabaseUser",
+	"$reportDatabasePassword", { RaiseError => 1 }
+);
+my $dbhStage = DBI->connect(
+	"dbi:DB2:$stageDatabase", "$stageDatabaseUser",
+	"$stageDatabasePassword", { RaiseError => 1 }
+);
 
-my $stmt = "Select id, name from eaadmin.bank_account where type = 'TADZ' with ur";
+my $stmt =
+  "Select id, name from eaadmin.bank_account where type = 'TADZ' with ur";
 my $sth = $dbhReporting->prepare($stmt);
 $sth->execute();
 my $bankAccountId;
 my $bankName;
 
-$sth->bind_col(1, \$bankAccountId);
-$sth->bind_col(2, \$bankName);
+$sth->bind_col( 1, \$bankAccountId );
+$sth->bind_col( 2, \$bankName );
 
 my %bankAccounts;
 
@@ -42,16 +48,18 @@ while ( $sth->fetch ) {
 
 my $bankList;
 
-while ( my ($bNum, $bName) = each %bankAccounts ) {
-		if (length($bankList) < 1 ) {
-			$bankList = $bNum;
-		} else {
-			$bankList .= ",$bNum";
-		}	
+while ( my ( $bNum, $bName ) = each %bankAccounts ) {
+	if ( length($bankList) < 1 ) {
+		$bankList = $bNum;
+	}
+	else {
+		$bankList .= ",$bNum";
+	}
 }
 
 $sth->finish;
-my $stmt2 = "select software_lpar.action,software_lpar.name,bios_serial,software_lpar.id, bank_account_id,software_lpar.customer_id from software_lpar, scan_record, software_lpar_map 
+my $stmt2 =
+"select software_lpar.action,software_lpar.name,bios_serial,software_lpar.id, bank_account_id,software_lpar.customer_id from software_lpar, scan_record, software_lpar_map 
 where software_lpar.id = software_lpar_map.software_lpar_id and
 scan_record.id = software_lpar_map.scan_record_id and
 bank_account_id in ($bankList) with ur;
@@ -65,61 +73,35 @@ my $serial;
 my $softwareLparId;
 my $thisBankAccountId;
 my $customerId;
-$sth2->bind_col(1, \$action);
-$sth2->bind_col(2, \$name);
-$sth2->bind_col(3, \$serial);
-$sth2->bind_col(4, \$softwareLparId);
-$sth2->bind_col(5, \$thisBankAccountId);
-$sth2->bind_col(6, \$customerId);
+$sth2->bind_col( 1, \$action );
+$sth2->bind_col( 2, \$name );
+$sth2->bind_col( 3, \$serial );
+$sth2->bind_col( 4, \$softwareLparId );
+$sth2->bind_col( 5, \$thisBankAccountId );
+$sth2->bind_col( 6, \$customerId );
 my %stageLpar;
-while ( $sth2->fetch) {
-	my $serial5 = substr($serial, -5, 5);
-	my $serial4 = substr($serial, -4, 4);
+
+while ( $sth2->fetch ) {
+	my $serial5 = substr( $serial, -5, 5 );
+	my $serial4 = substr( $serial, -4, 4 );
 	$stageLpar{$softwareLparId} = {
-		action => "$action",
-		name => "$name",
-		serial => "$serial", 
-		serial4 => "$serial4", 
-		serial5 => "$serial5",
+		action        => "$action",
+		name          => "$name",
+		serial        => "$serial",
+		serial4       => "$serial4",
+		serial5       => "$serial5",
 		bankAccountId => "$thisBankAccountId",
-		customerId => "$customerId",
-	};	
-#	print "$serial5 $serial4\n";
+		customerId    => "$customerId",
+	};
+
+	#	print "$serial5 $serial4\n";
 }
 $sth2->finish;
 
 $dbhStage->disconnect();
 
-my $stmt3 = "select id  from eaadmin.software_lpar where name = ? and customer_id = ? with ur";
-my $swLparId;
-my $swLparIdList = "";
-while ( my ($slNum, $swLpar) = each %stageLpar ) {
-	my $sth3 = $dbhReporting->prepare($stmt3);
-	$sth3->execute($stageLpar{$slNum}{name}, $stageLpar{$slNum}{customerId});
-	$sth3->bind_col(1, \$swLparId);
-	if ( $sth3->fetch ) {
-		$stageLpar{$slNum}{trailsSL} = $swLparId;
-		if (length($swLparIdList) < 2 ) {
-			$swLparIdList = $swLparId;
-		} else {
-			$swLparIdList .= ",$swLparId";
-		}	
-	} 
-	$sth3->finish;
-}
-
-my $stmt4 = "select eaadmin.software_lpar.status, eaadmin.software_lpar.name, bios_serial, scantime, account_number, 
-customer_name, match_method, eaadmin.software_lpar.id, 
-eaadmin.country_code.code, eaadmin.geography.name
-from eaadmin.customer, eaadmin.software_lpar 
-left outer join eaadmin.country_code on eaadmin.customer.country_code_id = eaadmin.country_code.id 
-left outer join eaadmin.region on eaadmin.country_code.region_id = eaadmin.region.id
-left outer join eaadmin.geography on eaadmin.geography.id = eaadmin.region.geography_id 
-left outer join eaadmin.hw_sw_composite on eaadmin.software_lpar.id = eaadmin.hw_sw_composite.software_lpar_id 
-where eaadmin.software_lpar.id in ($swLparIdList) and 
-eaadmin.software_lpar.customer_id = eaadmin.customer.customer_id with ur";
-
-$stmt4 = "select eaadmin.software_lpar.status, eaadmin.software_lpar.name, bios_serial, scantime, account_number,
+my $stmt3 =
+"select eaadmin.software_lpar.status, eaadmin.software_lpar.name, bios_serial, scantime, account_number,
 customer_name, match_method, eaadmin.software_lpar.id,
 eaadmin.country_code.code,
 eaadmin.geography.name
@@ -129,92 +111,96 @@ join eaadmin.customer on eaadmin.software_lpar.customer_id = eaadmin.customer.cu
 left join eaadmin.country_code on eaadmin.customer.country_code_id = eaadmin.country_code.id
 left outer join eaadmin.region on eaadmin.country_code.region_id = eaadmin.region.id
 left outer join eaadmin.geography on eaadmin.region.geography_id = eaadmin.geography.id
-where eaadmin.software_lpar.id in ($swLparIdList)
+where eaadmin.software_lpar.name = ? and eaadmin.software_lpar.customer_id = ?
 with ur";
 
-print $stmt4;
-my $sth4 = $dbhReporting->prepare($stmt4);
-my $status;
-my $LparName;
-my $biosSerial;
-my $scanTime;
-my $accountNumber;
-my $customerName;
-my $matchMethod;
-my $slId;
-my $countryCode;
-my $geography;
-$sth4->bind_col(1, \$status);
-$sth4->bind_col(2, \$LparName);
-$sth4->bind_col(3, \$biosSerial);
-$sth4->bind_col(4, \$scanTime);
-$sth4->bind_col(5, \$accountNumber);
-$sth4->bind_col(6, \$customerName);
-$sth4->bind_col(7, \$matchMethod);
-$sth4->bind_col(8, \$slId);
-$sth4->bind_col(9, \$countryCode);
-$sth4->bind_col(10, \$geography);
+#my $stmt3 = "select id  from eaadmin.software_lpar where name = ? and customer_id = ? with ur";
+while ( my ( $stageId, $swLpar ) = each %stageLpar ) {
+	my $sth3 = $dbhReporting->prepare($stmt3);
+	$sth3->execute( $stageLpar{$stageId}{name}, $stageLpar{$stageId}{customerId} );
+	my $status;
+	my $LparName;
+	my $biosSerial;
+	my $scanTime;
+	my $accountNumber;
+	my $customerName;
+	my $matchMethod;
+	my $slId;
+	my $countryCode;
+	my $geography;
+	my $matchMade;
+	$sth3->bind_col( 1,  \$status );
+	$sth3->bind_col( 2,  \$LparName );
+	$sth3->bind_col( 3,  \$biosSerial );
+	$sth3->bind_col( 4,  \$scanTime );
+	$sth3->bind_col( 5,  \$accountNumber );
+	$sth3->bind_col( 6,  \$customerName );
+	$sth3->bind_col( 7,  \$matchMethod );
+	$sth3->bind_col( 8,  \$slId );
+	$sth3->bind_col( 9,  \$countryCode );
+	$sth3->bind_col( 10, \$geography );
 
-$sth4->execute();
-my $matchMade;
-
-while ( $sth4->fetch ) {
-	if ( defined $matchMethod ) {
-		$matchMade = $matchMethod;
-	} else {
-		$matchMade = "NOT MATCHED";
-	}
-	my $stageId = lookupLpar( $slId);
-	if ( $stageId > 0 ) { 
-		$stageLpar{$stageId}{trailsStatus} = $status;
-		$stageLpar{$stageId}{trailsLparName} = $LparName;
-		$stageLpar{$stageId}{trailsBiosSerial} = $biosSerial;
-		$stageLpar{$stageId}{trailsScanTime} = $scanTime;
+	if ( $sth3->fetch ) {
+		if ( defined $matchMethod ) {
+			$matchMade = $matchMethod;
+		}
+		else {
+			$matchMade = "NOT MATCHED";
+		}
+		$stageLpar{$stageId}{trailsSL}              = $slId;
+		$stageLpar{$stageId}{trailsStatus}        = $status;
+		$stageLpar{$stageId}{trailsLparName}      = $LparName;
+		$stageLpar{$stageId}{trailsBiosSerial}    = $biosSerial;
+		$stageLpar{$stageId}{trailsScanTime}      = $scanTime;
 		$stageLpar{$stageId}{trailsAccountNumber} = $accountNumber;
-		$stageLpar{$stageId}{trailsCustomerName} = $customerName;
-		$stageLpar{$stageId}{trailsMatchMethod} = $matchMade;
+		$stageLpar{$stageId}{trailsCustomerName}  = $customerName;
+		$stageLpar{$stageId}{trailsMatchMethod}   = $matchMade;
 		if ( defined $countryCode && length($countryCode) > 1 ) {
 			$stageLpar{$stageId}{trailsCountryCode} = $countryCode;
-			$stageLpar{$stageId}{trailsGeography} = $geography;
-		} else {			
+			$stageLpar{$stageId}{trailsGeography}   = $geography;
+		}
+		else {
 			$stageLpar{$stageId}{trailsCountryCode} = "NA";
-			$stageLpar{$stageId}{trailsGeography} = "NA";
-			
-		}		
-	} else {
-		print "slI'd not found \n";	
+			$stageLpar{$stageId}{trailsGeography}   = "NA";
+
+		}
 	}
+	else {
+		print "$stageId not found\n";
+	}
+	$sth3->finish;
 }
 
-$sth4->finish;
 
 $dbhReporting->disconnect();
 
 open REPORT, ">$0" . ".txt";
 
-print REPORT "S-action,S-name,S-serial,S-serial4,S-serial5,S-bankAccountId,S-bankAccountName,T-Status,T-LparName," .
-"T-BiosSerial,T-ScanTime,T-AccountNumber,T-CustomerName,T-MatchMethod,T-CountryCode,T-Geography\n";
+print REPORT
+"S-action,S-name,S-serial,S-serial4,S-serial5,S-bankAccountId,S-bankAccountName,T-Status,T-LparName,"
+  . "T-BiosSerial,T-ScanTime,T-AccountNumber,T-CustomerName,T-MatchMethod,T-CountryCode,T-Geography\n";
 
 # print the information we have
-while ( my ($slNumPrint, $swLparPrint) = each %stageLpar ) {
+while ( my ( $slNumPrint, $swLparPrint ) = each %stageLpar ) {
 	print REPORT $stageLpar{$slNumPrint}{action} . ",";
 	print REPORT $stageLpar{$slNumPrint}{name} . ",";
 	print REPORT "\"" . $stageLpar{$slNumPrint}{serial} . "\",";
 	print REPORT $stageLpar{$slNumPrint}{serial4} . ",";
 	print REPORT $stageLpar{$slNumPrint}{serial5} . ",";
-	print REPORT $stageLpar{$slNumPrint}{bankAccountId} . ",";	
-	print REPORT $bankAccounts{$stageLpar{$slNumPrint}{bankAccountId}} . ",";
+	print REPORT $stageLpar{$slNumPrint}{bankAccountId} . ",";
+	print REPORT $bankAccounts{ $stageLpar{$slNumPrint}{bankAccountId} } . ",";
 	if ( defined $stageLpar{$slNumPrint}{trailsStatus} ) {
 		print REPORT $stageLpar{$slNumPrint}{trailsStatus} . ",";
 		print REPORT $stageLpar{$slNumPrint}{trailsLparName} . ",";
 		print REPORT $stageLpar{$slNumPrint}{trailsBiosSerial} . ",";
 		print REPORT $stageLpar{$slNumPrint}{trailsScanTime} . ",";
-		print REPORT $stageLpar{$slNumPrint}{trailsAccountNumber} . ",";			
-		print REPORT $stageLpar{$slNumPrint}{trailsCustomerName} . ",";			
-		print REPORT $stageLpar{$slNumPrint}{trailsMatchMethod}  . ",";			
-		print REPORT $stageLpar{$slNumPrint}{trailsCountryCode} . ",";			
-		print REPORT $stageLpar{$slNumPrint}{trailsGeography};			
-	} else {
+		print REPORT $stageLpar{$slNumPrint}{trailsAccountNumber} . ",";
+		print REPORT $stageLpar{$slNumPrint}{trailsCustomerName} . ",";
+		print REPORT $stageLpar{$slNumPrint}{trailsMatchMethod} . ",";
+		print REPORT $stageLpar{$slNumPrint}{trailsCountryCode} . ",";
+		print REPORT $stageLpar{$slNumPrint}{trailsGeography};
+	}
+	else {
 		print REPORT "NA,";
 		print REPORT "NA,";
 		print REPORT "NA,";
@@ -232,9 +218,10 @@ exit 0;
 
 sub lookupLpar {
 	my ($idToLookup) = shift @_;
-	while (my ($id, $Lpar) = each %stageLpar ) {
-		if ( (defined $stageLpar{$id}{trailsSL}) && 
-			($stageLpar{$id}{trailsSL} eq $idToLookup) ) {
+	while ( my ( $id, $Lpar ) = each %stageLpar ) {
+		if (   ( defined $stageLpar{$id}{trailsSL} )
+			&& ( $stageLpar{$id}{trailsSL} eq $idToLookup ) )
+		{
 			return $id;
 		}
 	}
@@ -246,5 +233,4 @@ sub usage {
 	print "$0 -u <userId>\n";
 	exit 0;
 }
-
 
