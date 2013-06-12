13ec9ccb16e049963e6a87ab09403773ce18cba1
zyizhang@cn.ibm.com <zyizhang@cn.ibm.com@c3b84367-c167-4d52-9215-bf13455bd1c0>
2013-06-12 14:24:31 +0000

    revert the change, wrongly applied to this module.
    git-svn-id: file:///Users/zhangyi/Documents/svnRepository/staging/trunk@205 c3b84367-c167-4d52-9215-bf13455bd1c0

--- Recon/AlertZeroProcessorCount.pm
+++ Recon/AlertZeroProcessorCount.pm
@@ -9,39 +9,247 @@ use Recon::OM::Alert;
 use Recon::OM::AlertHistory;
 use Recon::OM::AlertSoftwareLparNew;
 
-our @ISA = qw(Recon::AlertHWLpar);
+sub new {
+	my ( $class, $connection, $softwareLpar ) = @_;
+	my $self = {
+		_connection   => $connection,
+		_softwareLpar => $softwareLpar
+	};
+	bless $self, $class;
 
-sub needOpenAlert(){
-  my $self = shift;
- 
-   my $query = "
-select 
-h.chips 
-from 
-hardware h, hardware_lpar hl 
-where 
-h.id  = hl.hardware_id 
-and hl.id  = ?
-   ";
-   
-    my $procCount=undef;
-    $self->connection->prepareSqlQuery(('isChipsCountZero',$query));
-	my $sth = $self->connection->sql->{isChipsCountZero};
-	$sth->bind_columns( \$procCount );
-	$sth->execute( $self->hardwareLpar->id );
-	$sth->fetchrow_arrayref;
-	$sth->finish;
-	
-	if(defined $procCount && $procCount>0){
-	 return 0;
-	}else{
-	 return 1;
-	}
- 
+	$self->validate;
+
+	return $self;
 }
 
-sub getAlertTypeId(){
-  return 14;
+sub validate {
+	my $self = shift;
+
+	croak 'Connection is undefined'
+		unless defined $self->connection;
+
+	croak 'SoftwareLpar is undefined'
+		unless defined $self->softwareLpar;
+}
+
+sub recon {
+	my $self = shift;
+	ilog('Begin recon method of AlertZeroProcessorCount');
+
+	ilog('Acquiring Customer');
+	my $customer = new BRAVO::OM::Customer();
+	$customer->id( $self->softwareLpar->customerId );
+	$customer->getById( $self->connection );
+	dlog( $customer->toString );
+	ilog('Customer acquired');
+
+	###Setup validation object
+	my $validation = new Recon::Validation();
+	$validation->customer($customer);
+	$validation->softwareLpar( $self->softwareLpar );
+
+	###Open/Close alert based on validation
+	if (   $validation->isValidCustomer == 0
+		|| $validation->isValidSoftwareLpar == 0 )
+	{
+		$self->closeAlert(0);
+	}
+	elsif ( $self->isProcessorCountZero == 0 ) {
+		$self->closeAlert(0);
+	}
+	else {
+		$self->openAlert;
+	}
+}
+
+sub isProcessorCountZero {
+	my $self = shift;
+
+	my $procCount;
+	my $effCount;
+	$self->connection->prepareSqlQuery( $self->queryIsProcessorCountZero() );
+	my $sth = $self->connection->sql->{isProcessorCountZero};
+	$sth->bind_columns( \$procCount, \$effCount );
+	$sth->execute( $self->softwareLpar->id );
+	$sth->fetchrow_arrayref;
+	$sth->finish;
+
+	if ( defined $procCount && $procCount > 0 ) {
+		return 0;
+	}
+	if ( defined $effCount && $effCount > 0 ) {
+		return 0;
+	}
+	return 1;
+}
+
+sub queryIsProcessorCountZero {
+	my $query = '
+        select
+            sl.processor_count
+            ,sle.processor_count
+        from
+            software_lpar sl
+            left outer join software_lpar_eff sle on
+                sl.id = sle.software_lpar_id
+                and sle.status = \'ACTIVE\'
+        where
+            sl.id = ?
+    ';
+	return ( 'isProcessorCountZero', $query );
+}
+
+sub getAlert {
+	my ( $self, $alertType ) = @_;
+
+	my $id;
+	$self->connection->prepareSqlQuery( $self->queryAlertId() );
+	my $sth = $self->connection->sql->{alertId};
+	$sth->bind_columns( \$id );
+	$sth->execute( $self->softwareLpar->id, $alertType );
+	$sth->fetchrow_arrayref;
+	$sth->finish;
+	return $id;
+}
+
+sub queryAlertId {
+	my $query = '
+        select
+            a.id
+        from
+            alert_software_lpar asl
+            ,alert a
+            ,alert_type at
+        where
+            asl.software_lpar_id = ?
+            and at.code = ?
+            and asl.id = a.id
+            and a.alert_type_id = at.id
+    ';
+	return ( 'alertId', $query );
+}
+
+sub openAlert {
+	my $self = shift;
+	ilog('Begin OpenAlert method of AlertInvalidScanTime');
+	my $alertId = $self->getAlert('ZEROPROC');
+	my $alert   = new Recon::OM::Alert();
+	$alert->id($alertId);
+	$alert->getById( $self->connection );
+
+	return if ( defined $alert->customerId && $alert->open == 1 );
+
+	$self->recordHistory($alert) if defined $alert->customerId;
+
+	$alert->customerId( $self->softwareLpar->customerId );
+	$alert->alertTypeId(1);
+	$alert->alertCauseId(1);
+	$alert->open(1);
+	$alert->creationTime( currentTimeStamp() );
+	$alert->recordTime( currentTimeStamp() );
+	$alert->remoteUser('STAGING');
+	$alert->assignee(undef);
+	$alert->comment('Auto Open');
+	$alert->save( $self->connection );
+
+	my $softwareLparAlert = new Recon::OM::AlertSoftwareLparNew();
+	$softwareLparAlert->id( $alert->id );
+	$softwareLparAlert->getById( $self->connection );
+	return if defined $softwareLparAlert->softwareLparId;
+	$softwareLparAlert->softwareLparId( $self->softwareLpar->id );
+	$self->saveSoftwareLparAlert($softwareLparAlert);
+
+	ilog('OpenAlert method of AlertZeroProcessorCount complete');
+}
+
+sub closeAlert {
+	my ( $self, $save ) = @_;
+	ilog('Begin closeAlert method of AlertZeroProcessorCount');
+
+	my $alertId = $self->getAlert('ZEROPROC');
+	my $alert   = new Recon::OM::Alert();
+	$alert->id($alertId);
+	$alert->getById( $self->connection );
+
+	return if ( defined $alert->customerId && $alert->open == 0 );
+
+	if ( defined $alert->customerId ) {
+		$self->recordHistory($alert);
+		$save = 1;
+	}
+	else {
+		$alert->customerId( $self->softwareLpar->customerId );
+		$alert->alertTypeId(1);
+		$alert->alertCauseId(1);
+		$alert->creationTime( currentTimeStamp() );
+		$alert->recordTime( currentTimeStamp() );
+		$alert->remoteUser('STAGING');
+		$alert->assignee(undef);
+	}
+
+	$alert->comment('Auto Close');
+	$alert->open(0);
+	$alert->save( $self->connection ) if $save == 1;
+
+	my $softwareLparAlert = new Recon::OM::AlertSoftwareLparNew();
+	$softwareLparAlert->id( $alert->id );
+	$softwareLparAlert->getById( $self->connection );
+	return if defined $softwareLparAlert->softwareLparId;
+	$softwareLparAlert->softwareLparId( $self->softwareLpar->id );
+	$self->saveSoftwareLparAlert($softwareLparAlert) if $save == 1;
+
+	ilog('CloseAlert method of AlertZeroProcessorCount complete');
+}
+
+sub saveSoftwareLparAlert {
+	my ( $self, $softwareLparAlert ) = @_;
+
+	$self->connection->prepareSqlQuery( $self->insertSoftwareLparAlertNew );
+	my $sth = $self->connection->sql->{insertSoftwareLparAlertNew};
+	$sth->execute( $softwareLparAlert->id,
+		$softwareLparAlert->softwareLparId );
+	$sth->finish;
+}
+
+sub insertSoftwareLparAlertNew {
+	my $query = '
+        insert into
+            alert_software_lpar
+        values(?,?)
+    ';
+	return ( 'insertSoftwareLparAlertNew', $query );
+}
+
+sub recordHistory {
+	my ( $self, $alert ) = @_;
+	ilog('Begin recordHistory method of AlertZeroProcessorCount');
+
+	my $history = new Recon::OM::AlertHistory();
+	$history->alertId( $alert->id );
+	$history->customerId( $alert->customerId );
+	$history->alertTypeId( $alert->alertTypeId );
+	$history->alertCauseId( $alert->alertCauseId );
+	$history->open( $alert->open );
+	$history->creationTime( $alert->creationTime );
+	$history->recordTime( $alert->recordTime );
+	$history->remoteUser( $alert->remoteUser );
+	$history->assignee( $alert->assignee );
+	$history->comment( $alert->comment );
+	$history->save( $self->connection );
+
+	ilog('Complete recordHistory method of AlertZeroProcessorCount');
+}
+
+sub connection {
+	my $self = shift;
+	$self->{_connection} = shift if scalar @_ == 1;
+	return $self->{_connection};
+}
+
+sub softwareLpar {
+	my $self = shift;
+	$self->{_softwareLpar} = shift if scalar @_ == 1;
+	return $self->{_softwareLpar};
 }
 
 1;
