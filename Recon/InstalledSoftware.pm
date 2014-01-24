1dba8c5efc2d5af2e11fe567abf6ca5c1306c8e1
zyizhang@cn.ibm.com <zyizhang@cn.ibm.com@c3b84367-c167-4d52-9215-bf13455bd1c0>
2014-01-24 12:43:41 +0000

    bug fixing for #341
    git-svn-id: file:///Users/zhangyi/Documents/svnRepository/staging/trunk@369 c3b84367-c167-4d52-9215-bf13455bd1c0

--- Recon/InstalledSoftware.pm
+++ Recon/InstalledSoftware.pm
@@ -2779,13 +2779,13 @@ sub closeAlertUnlicensedSoftware {
 			$alert->save( $self->connection );
 			$self->recordAlertUnlicensedSoftwareHistory($oldAlert);
 		}
+		Recon::CauseCode::updateCCtable ( $alert->id, 17, $self->connection);
 	}
 	elsif ( $createNew == 1 ) {
 		$alert->creationTime( currentTimeStamp() );
 		$alert->save( $self->connection );
-	}
-	
-	Recon::CauseCode::updateCCtable ( $alert->id, 17, $self->connection);
+		Recon::CauseCode::updateCCtable ( $alert->id, 17, $self->connection);
+	}	
 
 	dlog("end closeAlertUnlicensedSoftware");
 }
