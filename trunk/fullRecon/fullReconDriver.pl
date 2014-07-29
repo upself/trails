df9cabed12dc27b1cb3d4f861619a3c3273ab1eb
Sasho_Mihailov@cz.ibm.com <Sasho_Mihailov@cz.ibm.com@506d4fb4-79cc-11e2-8167-7963c9775998>
2014-07-29 10:04:29 +0000

    Ticket #480   include 2 new columns
    git-svn-id: http://tapmf.boulder.ibm.com/svn/reports@141 506d4fb4-79cc-11e2-8167-7963c9775998

--- trunk/fullRecon/fullReconDriver.pl
+++ trunk/fullRecon/fullReconDriver.pl
@@ -142,6 +142,7 @@ ELSE 'Green' END
 ,h.owner as hwOwner
 ,h.country as hwCountry
 ,mt.type as hwAssetType
+,hl.server_type as Server_Type	
 ,hl.SPLA
 ,hl.VIRTUAL_FLAG
 ,hl.VIRTUAL_MOBILITY_RESTRICTION
@@ -202,6 +203,7 @@ when rt.is_manual = 1 then rt.name || '(MANUAL)' end
 ,l.prod_name as licProdName
 ,l.version as licVersion
 ,CONCAT(CONCAT(RTRIM(CHAR(L.Cap_Type)), '-'), CT.Description)
+,l.environment as Environment
 ,ul.used_quantity
 ,case when r.id is null then ''
 when r.machine_level = 0 then 'No' else 'Yes' end
