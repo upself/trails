5242a2e7d8819c8f32e0c7766a4f4ffb36c1c65c
Sasho_Mihailov@cz.ibm.com <Sasho_Mihailov@cz.ibm.com@506d4fb4-79cc-11e2-8167-7963c9775998>
2014-08-18 11:37:38 +0000

    http://tapmf.boulder.ibm.com/ticket/486#no3 driver for bank_acc reports created
    git-svn-id: http://tapmf.boulder.ibm.com/svn/reports@149 506d4fb4-79cc-11e2-8167-7963c9775998

---
+++ branches/sprint2014-07/bankAcc/all_bank.sh
@@ -0,0 +1,7 @@
+:
+
+/opt/reports/bin/bankAcc/all_bank_test.pl
+cp /opt/report/bin/bankAcc/all_bank.tsv /gsa/pokgsa/projects/e/emea_assets/all_bank.tsv
+cp /opt/report/bin/bankAcc/asset_bank.tsv /gsa/pokgsa/projects/e/emea_assets/tad4d_bank.tsv
+rm /opt/report/bin/bankAcc/all_bank.tsv
+rm /opt/report/bin/bankAcc/tad4d_bank.tsv
