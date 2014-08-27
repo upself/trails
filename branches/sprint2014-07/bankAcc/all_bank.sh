507df22ca34980318fbf614dab35e5dfb31b3675
Sasho_Mihailov@cz.ibm.com <Sasho_Mihailov@cz.ibm.com@506d4fb4-79cc-11e2-8167-7963c9775998>
2014-08-27 13:29:20 +0000

    update all_bank.sh to be include in chrontab http://tapmf.boulder.ibm.com/ticket/486
    git-svn-id: http://tapmf.boulder.ibm.com/svn/reports@155 506d4fb4-79cc-11e2-8167-7963c9775998

--- branches/sprint2014-07/bankAcc/all_bank.sh
+++ branches/sprint2014-07/bankAcc/all_bank.sh
@@ -1,7 +1,6 @@
 :
 
-/opt/reports/bin/bankAcc/all_bank_test.pl
-cp /opt/report/bin/bankAcc/all_bank.tsv /gsa/pokgsa/projects/e/emea_assets/all_bank.tsv
-cp /opt/report/bin/bankAcc/asset_bank.tsv /gsa/pokgsa/projects/e/emea_assets/tad4d_bank.tsv
-rm /opt/report/bin/bankAcc/all_bank.tsv
-rm /opt/report/bin/bankAcc/tad4d_bank.tsv
+/opt/reports/bin/all_bank_test.pl
+
+rm /tmp/T4DBankTmp2.txt 
+rm /tmp/AllBankTmp2.txt
