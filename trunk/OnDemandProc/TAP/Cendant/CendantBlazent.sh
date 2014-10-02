5b54146224ca5585e0abebb31274c10edec73270
zyizhang@cn.ibm.com <zyizhang@cn.ibm.com@506d4fb4-79cc-11e2-8167-7963c9775998>
2014-10-02 14:24:21 +0000

    merge sprint2014-08 into trunk
    git-svn-id: http://tapmf.boulder.ibm.com/svn/reports@191 506d4fb4-79cc-11e2-8167-7963c9775998

--- trunk/OnDemandProc/TAP/Cendant/CendantBlazent.sh
+++ trunk/OnDemandProc/TAP/Cendant/CendantBlazent.sh
@@ -9,9 +9,11 @@
 #dbName="ATP"
 #dbUser="ATBRAVO"
 #pw=`tail /gsa/pokgsa/projects/a/amsd/adp/OnDemandProc/TAP/kbin/atppw`
+reprop=$(uname -n |awk -F'.' '{print $1}').report.properties
+source /opt/report/bin/Conf/$reprop
 source /opt/staging/v2/config/connectionConfig.txt
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
-. /db2/tap/sqllib/db2profile;
+. $db2profile;
 db2 connect to $ATPdb user $ATPuser using $ATPpassword;
 WDIR="/gsa/pokgsa/projects/a/amsd/adp/OnDemandProc/TAP"
 db2 -tf $WDIR/Cendant/CendantBlazent.sql | $WDIR/kbin/sql2tsv 1> /var/http_reports/Cendant_ATPBlazent.xls;
