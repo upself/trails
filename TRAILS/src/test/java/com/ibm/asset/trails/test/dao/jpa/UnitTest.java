c325a349e9fad7d2b1fba36b9d9326e04f158f96
liuhaidl@cn.ibm.com <liuhaidl@cn.ibm.com@35984d28-f107-47b3-a003-c646fc333b22>
2014-10-07 14:16:22 +0000

    JUnit Test Case Support Sample Test
    git-svn-id: file:///Users/zhangyi/Documents/svn/trails/trunk@256 35984d28-f107-47b3-a003-c646fc333b22

---
+++ TRAILS/src/test/java/com/ibm/asset/trails/test/dao/jpa/UnitTest.java
@@ -0,0 +1,18 @@
+package com.ibm.asset.trails.test.dao.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void testEqualsTrue() {
+	  assertEquals(5, 5);
+	}
+
+	@Test
+	public void testEqualsFalse() {
+	  assertEquals(3, 5);
+	}
+}
