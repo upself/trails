11b70cdcfb4163a08fdaf71a20b5ec6865926ef3
Ondrej Zivnustka <ondrej_zivnustka@cz.ibm.com>
2014-12-16 09:01:35 +0100

    test comments edit

--- staging/t/tests/ATPDelegate/processorCountLogic.pm
+++ staging/t/tests/ATPDelegate/processorCountLogic.pm
@@ -15,13 +15,13 @@ sub offline_test_1 : Tests(8) {
 	my $test  = shift;
 	my $class = $test->class;
 	
-	is($class->processorCountLogic(undef),undef, "test offline undef => undef");
-	is($class->processorCountLogic(''),undef, "test offline '' => undef");	
-	is($class->processorCountLogic(0),undef, "test offline 0 => undef");
-	is($class->processorCountLogic(1),1, "test offline 1 => undef");
-	is($class->processorCountLogic(12.1231),12, "test offline 12.1231 => 12");
-	is($class->processorCountLogic(0.1241),undef, "test offline 0.1241 => undef");
-	is($class->processorCountLogic(-1.1241),undef, "test offline -1.1241 => undef");
-	is($class->processorCountLogic(-6),undef, "test offline -6 => undef");
+	is($class->processorCountLogic(undef),undef, "processorCountLogic effProc undef => undef");
+	is($class->processorCountLogic(''),undef, "processorCountLogic effProc '' => undef");	
+	is($class->processorCountLogic(0),undef, "processorCountLogic effProc 0 => undef");
+	is($class->processorCountLogic(1),1, "processorCountLogic effProc 1 => undef");
+	is($class->processorCountLogic(12.1231),12, "processorCountLogic effProc 12.1231 => 12");
+	is($class->processorCountLogic(0.1241),undef, "processorCountLogic effProc 0.1241 => undef");
+	is($class->processorCountLogic(-1.1241),undef, "processorCountLogic effProc -1.1241 => undef");
+	is($class->processorCountLogic(-6),undef, "processorCountLogic effProc -6 => undef");
 }
 1;
