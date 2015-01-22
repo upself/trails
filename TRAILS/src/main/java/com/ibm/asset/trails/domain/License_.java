711c3556922efcfdbcdc7ab2950aed1efa751eae
Louis <zyizhang@cn.ibm.com>
2015-01-22 15:18:55 +0800

    Story 17231, feature added. allow MLA on HW IFL

--- TRAILS/src/main/java/com/ibm/asset/trails/domain/License_.java
+++ TRAILS/src/main/java/com/ibm/asset/trails/domain/License_.java
@@ -29,4 +29,5 @@ public class License_ {
 	public static volatile SetAttribute<License, UsedLicenseHistory> usedLicenseHistories;
 	public static volatile SingularAttribute<License, Integer> pool;
 	public static volatile SingularAttribute<License, String> environment;
+	public static volatile SingularAttribute<License, String> licenseType;
 }
