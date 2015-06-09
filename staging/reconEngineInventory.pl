bac7d06691bb7638868c1094b8ba24cfe7fcf457
myyysha <michal.starek@cz.ibm.com>
2015-06-09 15:46:14 +0200

    added properties for recon engine stop and monitoring

--- staging/reconEngineInventory.pl
+++ staging/reconEngineInventory.pl
@@ -112,11 +112,15 @@ sub keepTicking {
             }
         }
         
-        if(scalar @customerIds == 0){
+        if ((scalar @customerIds == 0) && ( $children > 0 )) {
             wlog("$rNo loop of customer array finished will sleep till end of child");
             sleep;
             wlog("$rNo waked up");   
-        }    
+        }
+        elsif ((scalar @customerIds == 0) && ( $children == 0 ) && ( loaderCheckForStop ( $pidFile ) == 0 )) {
+			wlog("$rNo customer array empty and no children running, waiting 10 minutes before reloading the queue");
+			sleep 600;
+		}
     }
 }
 
