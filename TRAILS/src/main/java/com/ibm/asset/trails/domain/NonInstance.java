b70f686c5e669c416b523f7bd3efa0da84afdce9
Tony Zhang <zhysz@cn.ibm.com>
2015-06-15 22:42:20 +0800

    phase 6 commit for import feature of story branch

--- TRAILS/src/main/java/com/ibm/asset/trails/domain/NonInstance.java
+++ TRAILS/src/main/java/com/ibm/asset/trails/domain/NonInstance.java
@@ -21,6 +21,7 @@ import javax.persistence.Table;
 	          ,@NamedQuery(name = "findNonInstancesByManufacturerId", query = "from NonInstance nonInstance where nonInstance.manufacturer.id = :manufacturerId")
 	          ,@NamedQuery(name = "findNonInstancesById", query = "from NonInstance nonInstance where nonInstance.id = :id") 
 	          ,@NamedQuery(name = "findNonInstancesBySwIdAndCapacityCode", query = "from NonInstance nonInstance where nonInstance.software.softwareId = :softwareId and nonInstance.capacityType.code = :code")
+              ,@NamedQuery(name = "findNonInstancesBySwNameAndCapacityCode", query = "from NonInstance nonInstance where nonInstance.software.softwareName = :softwareName and nonInstance.capacityType.code = :code")
 	          ,@NamedQuery(name = "findNonInstancesBySwIdAndCapacityCodeNotEqId", query = "from NonInstance nonInstance where nonInstance.software.softwareId = :softwareId and nonInstance.capacityType.code = :code and nonInstance.id != :id") 
 	          ,@NamedQuery(name = "removeNonInstanceById", query = "update NonInstance nonInstance set nonInstance.status.id = 1 where id = :id")})
 public class NonInstance extends AbstractDomainEntity {
