e3d63a05e35a6ea3d4b086ef69fb90ad866141af
Ondrej Zivnustka <ondrej_zivnustka@cz.ibm.com>
2015-06-15 09:30:40 +0200

    != / ne

--- staging/BRAVO/Loader/HardwareLpar.pm
+++ staging/BRAVO/Loader/HardwareLpar.pm
@@ -123,9 +123,9 @@ sub logic {
 				$bravoHardware->getById( $self->bravoConnection );
 				dlog( $bravoHardware->toString );
 				
-				if (( $self->bravoHardware->serial != $bravoHardware->serial )
+				if (( $self->bravoHardware->serial ne $bravoHardware->serial )
 					|| ( $self->bravoHardware->machineTypeId != $bravoHardware->machineTypeId )
-					|| ( $self->bravoHardware->country != $bravoHardware->country ))
+					|| ( $self->bravoHardware->country ne $bravoHardware->country ))
 				{
 					$self->reconDeep(1);
 					$self->reconHardware($bravoHardware);
