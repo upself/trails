f5d9944442dc93d6657189fdf7ef32696f68894d
Larry Liu <liuhaidl@cn.ibm.com>
2015-08-07 18:26:55 +0800

    Design and Implement Recon Priority ISV SW new feature

---
+++ TRAILS/src/main/java/com/ibm/asset/trails/dao/jpa/ReconPriorityISVSoftwareDAOJap.java
@@ -0,0 +1,42 @@
+package com.ibm.asset.trails.dao.jpa;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ibm.asset.trails.dao.ReconPriorityISVSoftwareDAO;
+import com.ibm.asset.trails.domain.ReconPriorityISVSoftware;
+
+@Repository
+public class ReconPriorityISVSoftwareDAOJap extends
+		AbstractGenericEntityDAOJpa<ReconPriorityISVSoftware, Long> implements
+		ReconPriorityISVSoftwareDAO {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ReconPriorityISVSoftware findReconPriorityISVSoftwareByUniqueKeys(
+			Long manufacturerId, Long customerId) {
+		List<ReconPriorityISVSoftware> results;
+		 
+		if(customerId == null){
+	       results = entityManager
+		                .createNamedQuery("findReconPriorityISVSoftwareByUniqueKeys1")
+		                .setParameter("manufacturerId", manufacturerId).getResultList();
+	    }
+		else{
+		   results = entityManager
+	                .createNamedQuery("findReconPriorityISVSoftwareByUniqueKeys2")
+	                .setParameter("customerId", customerId)
+	                .setParameter("manufacturerId", manufacturerId)
+	                .getResultList();
+		}
+		
+	    ReconPriorityISVSoftware result;
+        if (results == null || results.isEmpty()) {
+            result = null;
+        } else {
+            result = results.get(0);
+        }
+        return result;
+	}
+}
