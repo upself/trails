f1f3e00fd6a4c707e99f0bb86005b9af6258140a
Ondrej Zivnustka <ondrej_zivnustka@cz.ibm.com>
2015-08-14 14:19:22 +0200

    dont use white characters but write_string function :D

--- reports/swMultiReport/bravoReport.pl
+++ reports/swMultiReport/bravoReport.pl
@@ -467,7 +467,7 @@ my $lineCount = 2;
 			$bravoSoftware->{$accountNumber}->{$hostname}->{'chipCount'} );
 		$heartbeat->write( $lineCount, 5,
 			$bravoSoftware->{$accountNumber}->{$hostname}->{'osName'} );
-		$heartbeat->write( $lineCount, 6, $osVersion . "\f" );
+		$heartbeat->write_string( $lineCount, 6, $osVersion );
 		$heartbeat->write( $lineCount, 7,
 			$bravoSoftware->{$accountNumber}->{$hostname}->{'osMinorVers'} );
 		$heartbeat->write( $lineCount, 8,
@@ -574,8 +574,7 @@ sub createSoftwareSheet {
 				$bravoSoftware->{$accountNumber}->{$hostname}->{'scantime'} );
 			$software->write( $lineCount, 4,
 				$bravoSoftware->{$accountNumber}->{$hostname}->{'osName'} );
-			$software->write( $lineCount, 5,
-				$osVersion . "\f" );
+			$software->write_string( $lineCount, 5,$osVersion);
 			$software->write( $lineCount, 6,
 				$bravoSoftware->{$accountNumber}->{$hostname}
 				  ->{'processorCount'} );
@@ -706,8 +705,7 @@ sub createSoftwareSheet {
 					  ->{'scantime'} );
 				$software->write( $lineCount, 4,
 					$bravoSoftware->{$accountNumber}->{$hostname}->{'osName'} );
-				$software->write( $lineCount, 5,
-					$osVersion . "\f");
+				$software->write_string( $lineCount, 5,$osVersion);
 				$software->write( $lineCount, 6,
 					$bravoSoftware->{$accountNumber}->{$hostname}
 					  ->{'processorCount'} );
