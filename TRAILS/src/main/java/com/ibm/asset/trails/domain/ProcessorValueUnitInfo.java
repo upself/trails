10a6333337291e88a9b4eec496a43a0f73204d9b
liuhaodl <dllhao@cn.ibm.com>
2015-08-20 14:28:04 +0800

    remove unuseful import class

--- TRAILS/src/main/java/com/ibm/asset/trails/domain/ProcessorValueUnitInfo.java
+++ TRAILS/src/main/java/com/ibm/asset/trails/domain/ProcessorValueUnitInfo.java
@@ -10,8 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.junit.Ignore;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
