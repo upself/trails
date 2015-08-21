5bbf65c205fae9a6b50e47bafe699483d8f1b1b6
Larry Liu <liuhaidl@cn.ibm.com>
2015-08-21 17:40:56 +0800

    support SOM3 format cause code upload function

--- TRAILS/src/main/java/com/ibm/asset/trails/service/impl/CauseCodeServiceImpl.java
+++ TRAILS/src/main/java/com/ibm/asset/trails/service/impl/CauseCodeServiceImpl.java
@@ -28,6 +28,8 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ibm.asset.trails.domain.AlertCause;
+import com.ibm.asset.trails.domain.AlertUnlicensedSw;
+import com.ibm.asset.trails.domain.AlertUnlicensedSwH;
 import com.ibm.asset.trails.domain.CauseCode;
 import com.ibm.asset.trails.domain.CauseCodeHistory;
 import com.ibm.asset.trails.service.AccountService;
@@ -60,6 +62,8 @@ public class CauseCodeServiceImpl implements CauseCodeService {
 
 	private final static String STEP2_LABEL = "VALIDATE";
 	private final static String STEP3_LABEL = "PERSISTE";
+	
+	private static final String SOM3_REPORT_NAME = "SOM3: SW INSTANCES WITH DEFINED CONTRACT SCOPE";
 
 	@Autowired
 	private AccountService accountService;
@@ -411,7 +415,19 @@ public class CauseCodeServiceImpl implements CauseCodeService {
 			} catch (Exception e) {
 				log.error(e.getMessage(), e);
 			}
-
+			
+			if(colIndexes.getColAssigneeComments()!=-1){
+				HSSFCell assigneeCommentsCell = row.getCell(colIndexes.getColAssigneeComments());
+	
+				String assigneeComments = "";
+				if (assigneeCommentsCell.getCellType() == HSSFCell.CELL_TYPE_STRING) {
+				  assigneeComments = assigneeCommentsCell.getStringCellValue();
+				} 
+				
+			    if(assigneeComments!=null&&!"".equals(assigneeComments.trim())){
+			      updateAssigneeComments(causeCode.getAlertId(), assigneeComments.trim(), colIndexes.getReportName().trim(),remoteUser);
+			    }
+			}
 		}
 
 	}
@@ -451,7 +467,7 @@ public class CauseCodeServiceImpl implements CauseCodeService {
 			return false;
 		}
 
-		return dbValue.equals(cellValue);
+		return dbValue.trim().equals(cellValue.trim());
 	}
 
 	private void buildErrorMsg(StringBuffer result, int columnNo,
@@ -555,5 +571,50 @@ public class CauseCodeServiceImpl implements CauseCodeService {
 				.createNamedQuery("getCauseCodeById")
 				.setParameter("id", causeCodeId).getResultList();
 		return causeCode;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void updateAssigneeComments(long alertId, String assigneeComments, String reportName, String remoteUser){
+	   if(reportName!=null&& !"".equals(reportName.trim())){
+    	  if(reportName.equalsIgnoreCase(SOM3_REPORT_NAME)){
+    	     List<AlertUnlicensedSw> ausList = null;
+    			try {
+    				ausList = getEntityManager()
+    						.createNamedQuery("alertUnlicensedSwListById")
+    						.setParameter("idList",
+    								alertId)
+    						.getResultList();
+    			} catch (Exception e) {
+    				log.error(e.getMessage(), e);
+    			}
+    			
+    		  if(ausList.size() >= 1){
+    			 AlertUnlicensedSw ausInDB = ausList.get(0);
+    			 String assigneeCommentsInDB = ausInDB.getComments();
+    			 if(!strCompare(assigneeCommentsInDB,assigneeComments)){
+    			 
+	    			 AlertUnlicensedSwH ausHistory = new AlertUnlicensedSwH();
+	    			 ausHistory.setAlertUnlicensedSw(ausInDB);
+	    			 ausHistory.setComments(ausInDB.getComments());
+	    			 ausHistory.setRemoteUser(ausInDB.getRemoteUser());
+	    			 ausHistory.setCreationTime(ausInDB.getCreationTime());
+	    			 ausHistory.setRecordTime(ausInDB.getRecordTime());
+	    			 ausHistory.setOpen(ausInDB.isOpen());
+	    			 
+	    			 ausInDB.setComments(assigneeComments);
+	    			 ausInDB.setRemoteUser(remoteUser);
+	    			 ausInDB.setRecordTime(new Date());
+	    			 
+	    			 try{
+						getEntityManager().persist(ausHistory);
+						getEntityManager().persist(ausInDB);
+						getEntityManager().flush();
+	    			  } catch (Exception e) {
+	    					log.error(e.getMessage(), e);
+	    			 }
+    			 }
+    		  }
+    	  } 
+       }			
 	}
 }
