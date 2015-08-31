b471cc0a3d6706b3de6f05df9b6258f6032df1fc
Larry Liu <liuhaidl@cn.ibm.com>
2015-08-31 15:06:27 +0800

    Cause code upload file structure adjustment for SOM4a, 4b, 4c

--- TRAILS/src/main/java/com/ibm/asset/trails/service/impl/ECauseCodeReport.java
+++ TRAILS/src/main/java/com/ibm/asset/trails/service/impl/ECauseCodeReport.java
@@ -6,14 +6,15 @@ public enum ECauseCodeReport
 	// 1. Report Name|2. Alert Type Id |3. Assignee|4. Assignee comments|5. Assigned date/time|
 	// 6. Cause Code (CC)|7. CC target date|8. CC owner|9. CC change date|10. CC change person|
 	// 11. CC Internal ID|12. message
-	NOLICIBM("Unlicensed IBM SW alert report", 17, -1, -1, -1, 6, 7, 8, 9, 10, 11, 12),
-	NOLICISV("Unlicensed ISV SW alert report", 17, -1, -1, -1, 6, 7, 8, 9, 10, 11, 12),
 	HARDWARE("HW w/o HW LPAR alert report", 3, -1, -1, -1, 9, 10, 11, 12, 13, 14, 15),
 	HW_LPAR("HW LPAR w/o SW LPAR alert report", 4, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16),
 	SW_LPAR("SW LPAR w/o HW LPAR alert report", 5, -1, -1, -1, 8, 9, 10, 11, 12, 13, 14),
 	EXP_SCAN("Outdated SW LPAR alert report", 6, -1, -1, -1,  8, 9, 10, 11, 12, 13, 14), 
 	SOM1B("SOM1b: HW BOX CRITICAL CONFIGURATION DATA POPULATED", 37, -1, -1, -1, 19, 20, 21, 22, 23, 24, 25),
 	SOM3("SOM3: SW INSTANCES WITH DEFINED CONTRACT SCOPE", 57, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+	SOM4A("SOM4a: IBM SW INSTANCES REVIEWED", 7, -1, -1, -1, 8, 9, 10, 11, 12, 13, 14),
+	SOM4B("SOM4b: PRIORITY ISV SW INSTANCES REVIEWED", 58, -1, -1, -1, 8, 9, 10, 11, 12, 13, 14),
+    SOM4C("SOM4c: ISV SW INSTANCES REVIEWED", 59, -1, -1, -1, 8, 9, 10, 11, 12, 13, 14),
 
 	private String reportName;
 	private long alertTypeId;
