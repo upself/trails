3f933073cbf866a4a32fbf7a6d832bba52b94344
myyysha <michal.starek@cz.ibm.com>
2015-09-09 15:04:24 +0200

    altered the sleep period

--- staging/reconEngineInventory.pl
+++ staging/reconEngineInventory.pl
@@ -161,7 +161,7 @@ sub newChild {
     my $reconEngine = new Recon::InventoryReconEngineCustomer( $customerId, $date, $poolRunning );
     $reconEngine->recon;
 
-    sleep 5;
+    sleep 15;
     wlog("$rNo Child $customerId, $date, $poolRunning complete");
 
     exit;
