333cb04bb64d6fd65cd0b3afc13bc65b08102b0d
myyysha <michal.starek@cz.ibm.com>
2015-09-09 16:54:31 +0200

    updated the sleep value

--- staging/reconEngineInventory.pl
+++ staging/reconEngineInventory.pl
@@ -161,7 +161,7 @@ sub newChild {
     my $reconEngine = new Recon::InventoryReconEngineCustomer( $customerId, $date, $poolRunning );
     $reconEngine->recon;
 
-    sleep 15;
+    sleep 12;
     wlog("$rNo Child $customerId, $date, $poolRunning complete");
 
     exit;
