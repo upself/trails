7398b87515aff886b6d33110c44e5e9d5a1ca52f
myyysha <michal.starek@cz.ibm.com>
2015-09-10 11:23:47 +0200

    last attempt of sleep

--- staging/reconEngineInventory.pl
+++ staging/reconEngineInventory.pl
@@ -161,7 +161,7 @@ sub newChild {
     my $reconEngine = new Recon::InventoryReconEngineCustomer( $customerId, $date, $poolRunning );
     $reconEngine->recon;
 
-    sleep 12;
+    sleep 5;
     wlog("$rNo Child $customerId, $date, $poolRunning complete");
 
     exit;
