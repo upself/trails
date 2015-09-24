ef60fdf9b00a23f558dd683ea6f615215204c133
liuhaodl <dllhao@cn.ibm.com>
2015-09-24 17:25:18 +0800

    correct delete sql of history table

--- deployment/2015_sprint15/alert_hardware_cfgdata.sql
+++ deployment/2015_sprint15/alert_hardware_cfgdata.sql
@@ -1,4 +1,4 @@
 --this is for story 32034:Run one time script to reset aging on SOM1b
 update alert_hardware_cfgdata set creation_time=current timestamp;
 update alert_hardware_cfgdata set record_time=current timestamp;
-delete table alert_hardware_cfgdata_h;
+delete from alert_hardware_cfgdata_h;
