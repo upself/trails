371ad04969e7942fe4b6b8b94d82c4c2adcb341a
liuhaodl <dllhao@cn.ibm.com>
2015-09-24 20:27:02 +0800

    add schema eaadmin ahead of the table name

--- deployment/2015_sprint15/alert_hardware_cfgdata.sql
+++ deployment/2015_sprint15/alert_hardware_cfgdata.sql
@@ -1,4 +1,4 @@
 --this is for story 32034:Run one time script to reset aging on SOM1b
-update alert_hardware_cfgdata set creation_time=current timestamp;
-update alert_hardware_cfgdata set record_time=current timestamp;
-delete from alert_hardware_cfgdata_h;
+update eaadmin.alert_hardware_cfgdata set creation_time=current timestamp;
+update eaadmin.alert_hardware_cfgdata set record_time=current timestamp;
+delete from eaadmin.alert_hardware_cfgdata_h;
