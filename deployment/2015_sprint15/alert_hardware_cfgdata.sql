6cabeb5858670b2b27cdacc3a362d585977b4ede
liuhaodl <dllhao@cn.ibm.com>
2015-09-24 17:15:31 +0800

    add additional sql to update record time to current time

--- deployment/2015_sprint15/alert_hardware_cfgdata.sql
+++ deployment/2015_sprint15/alert_hardware_cfgdata.sql
@@ -1,3 +1,4 @@
 --this is for story 32034:Run one time script to reset aging on SOM1b
-update alert_hardware_cfgdata set creation_time=current timestamp where open=1;
+update alert_hardware_cfgdata set creation_time=current timestamp;
+update alert_hardware_cfgdata set record_time=current timestamp;
 delete table alert_hardware_cfgdata_h;
