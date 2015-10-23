9be7f9887ab94e73058d0e12c2a7f545f4e5daf5
Louis <zyizhang@cn.ibm.com>
2015-10-23 16:13:44 +0800

    Story 35899, add ddl of new tables

---
+++ deployment/2015_sprint16/Story_35899/SCARLET_RECONCILE_ddl_st.sql
@@ -0,0 +1,39 @@
+DROP TABLE EAADMIN.SCARLET_RECONCILE
+;
+------------------------------------------------
+-- DDL Statements for table "EAADMIN "."SCARLET_RECONCILE"
+------------------------------------------------
+CREATE TABLE "EAADMIN"."SCARLET_RECONCILE" (
+		"ID" BIGINT NOT NULL,
+		"LAST_VALIDATE_TIME" TIMESTAMP NOT NULL
+	)
+                 DATA CAPTURE CHANGES
+                 IN "RECON" INDEX IN "RECONINDEX" ;
+------------------------------------------------
+-- DDL Statements for table "EAADMIN "."SCARLET_RECONCILE"
+------------------------------------------------
+COMMIT WORK;
+
+-- DDL Statements for indexes on Table "EAADMIN "."SCARLET_RECONCILE"
+
+CREATE UNIQUE INDEX "EAADMIN "."PKSCARLETRECONCILE11" ON "EAADMIN "."SCARLET_RECONCILE"
+                ("ID" ASC) CLUSTER ALLOW REVERSE SCANS;
+                
+-- DDL Statements for indexes on Table "EAADMIN "."SCARLET_RECONCILE"
+
+CREATE INDEX "EAADMIN "."IDXSCARLETRECONCILE11" ON "EAADMIN "."SCARLET_RECONCILE"
+                ("ID" ASC, 
+                "LAST_VALIDATE_TIME" ASC) ALLOW REVERSE SCANS;               
+             
+-- DDL Statements for primary key on Table "EAADMIN "."SCARLET_RECONCILE"
+
+ALTER TABLE "EAADMIN "."SCARLET_RECONCILE"
+        ADD CONSTRAINT "CSSCARLETRECONCILE" PRIMARY KEY
+                ("ID");
+
+COMMIT WORK;
+
+---------- Privilege For SCARLET_RECONCILE ----------
+GRANT REFERENCES ON TABLE "EAADMIN "."SCARLET_RECONCILE"      TO GROUP "TRAILUPD" ;       
+GRANT SELECT ON TABLE "EAADMIN "."SCARLET_RECONCILE"          TO GROUP "TRAILSTG" ; 
+COMMIT WORK;
