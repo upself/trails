f88de25ce313c667ed6a482ba74626c4c6ecf1d8
Tony Zhang <zhysz@cn.ibm.com>
2015-12-04 16:58:48 +0800

    remove ostype from som1b

--- reports/tapreports/GSA_projects_a_amsd_adp_OnDemandProc_Global_OpenAlerts_new/r3_RunEMEA.sh
+++ reports/tapreports/GSA_projects_a_amsd_adp_OnDemandProc_Global_OpenAlerts_new/r3_RunEMEA.sh
@@ -18,16 +18,6 @@ rts=`date`
 #5 Unlicensed IBM SW alert
 #6 Unlicensed ISV SW alert
 
-GCG1="GCG1_SOM1a_HW_WITH_HOSTNAME"
-GCG2="GCG2_SOM2a_HW_LPAR_with_SW_LPAR"
-GCG3="GCG3_SOM2b_SW_LPAR_with_HW_LPAR"
-GCG4="GCG4_SOM2c_Unexpired_SW_LPAR"
-GCG5="GCG5_SOM1b_HW_BOX_CRITICAL_CONFIGURATION_DATA_POPULATED"
-GCG6="GCG6_SOM3_SW_INSTANCES_WITH_DEFINED_CONTRACT_SCOPE"
-GCG7="GCG7_SOM4a_IBM_SW_INSTANCES_REVIEWED"
-GCG8="GCG8_SOM4b_PRIORITY_ISV_SW_INSTANCES_REVIEWED"
-GCG9="GCG9_SOM4c_ISV_SW_INSTANCES_REVIEWED"
-
 MEA1="MEA1_SOM1a_HW_WITH_HOSTNAME"
 MEA2="MEA2_SOM2a_HW_LPAR_with_SW_LPAR"
 MEA3="MEA3_SOM2b_SW_LPAR_with_HW_LPAR"
@@ -45,38 +35,6 @@ MEA9="MEA9_SOM4c_ISV_SW_INSTANCES_REVIEWED"
 
 db2 connect to ${dbName} user ${dbUser} using ${pw}
 db2 set schema eaadmin
-
-db2 -tf GCG1.sql | sql2tsv 1> $GCG1.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG1.txt
-db2 -tf GCG2.sql | sql2tsv 1> $GCG2.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG2.txt
-db2 -tf GCG3.sql | sql2tsv 1> $GCG3.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG3.txt
-db2 -tf GCG4.sql | sql2tsv 1> $GCG4.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG4.txt
-db2 -tf GCG5.sql | sql2tsv 1> $GCG5.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG5.txt
-db2 -tf GCG6.sql | sql2tsv 1> $GCG6.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG6.txt
-db2 -tf GCG7.sql | sql2tsv 1> $GCG7.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG7.txt
-db2 -tf GCG8.sql | sql2tsv 1> $GCG8.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG8.txt
-db2 -tf GCG9.sql | sql2tsv 1> $GCG9.txt;
-echo "IBM Confidential: Report Created $(date), $rts" >> $GCG9.txt
-zip -m $GCG1.zip $GCG1.txt
-zip -m $GCG2.zip $GCG2.txt
-zip -m $GCG3.zip $GCG3.txt
-zip -m $GCG4.zip $GCG4.txt
-zip -m $GCG5.zip $GCG5.txt
-zip -m $GCG6.zip $GCG6.txt
-zip -m $GCG7.zip $GCG7.txt
-zip -m $GCG8.zip $GCG8.txt
-zip -m $GCG9.zip $GCG9.txt
-scp *.zip /gsa/pokgsa/projects/a/amsd/adp/test
-rm *.zip
-chmod 0664 /gsa/pokgsa/projects/a/amsd/adp/test/*.zip
-
 
 db2 -tf MEA1.sql | sql2tsv 1> $MEA1.txt;
 echo "IBM Confidential: Report Created $(date), $rts" >> $MEA1.txt
