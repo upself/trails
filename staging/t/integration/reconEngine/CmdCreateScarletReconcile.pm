2aa9faee89a204de96588771645116c3e740d029
Louis <zyizhang@cn.ibm.com>
2016-01-15 15:45:00 +0800

    Sotry 39148, change directory structure. 2

---
+++ staging/t/integration/reconEngine/CmdCreateScarletReconcile.pm
@@ -0,0 +1,33 @@
+package integration::reconEngine::CmdCreateScarletReconcile;
+
+use strict;
+
+use Database::Connection;
+use Recon::OM::ScarletReconcile;
+
+sub new {
+ my ( $class, $id, $time ) = @_;
+ my $self = {
+  _id         => $id,
+  _time       => $time,
+  _connection => Database::Connection->new('trails')
+
+ };
+ bless $self, $class;
+
+ return $self;
+}
+
+sub execute {
+ my $self = shift;
+
+ my $connection = $self->{_connection};
+
+ my $r = Recon::OM::ScarletReconcile->new();
+ $r->id( $self->{_id} );
+ $r->lastValidateTime( $self->{_time} );
+ $r->save($connection);
+
+}
+
+1;
---
+++ staging/t/integration/reconEngine/Tests/TestEndpoint.pm
@@ -0,0 +1,149 @@
+package integration::reconEngine::Tests::TestEndpoint;
+
+use base qw(
+  Test::Class
+  integration::ScarletAPIManager
+  integration::LogManager
+);
+
+use strict;
+use Test::More;
+use Test::File;
+
+use Scarlet::GuidEndpoint;
+
+sub _startup : Test(startup) {
+ my $self  = shift;
+ my $class = ref($self);
+ diag("---start of $class---");
+}
+
+sub setupLog : Test(startup) {
+ my $self = shift;
+
+ $self->{logFile} = '/tmp/testEndpoint.txt';
+ $self->configDebugLevel( $self->{logFile} );
+
+ $self->{connectionFile} = '/opt/staging/v2/config/connectionConfig.txt';
+}
+
+sub shutdown : Test(shutdown) {
+ my $self = shift;
+
+ $self->resetGuid;
+ $self->deleteLogFile;
+}
+
+sub cleanLogFile : Test(setup) {
+ my $self = shift;
+ $self->deleteLogFile;
+}
+
+sub story38372_checkOutOfService : Test(2) {
+ my $self = shift;
+
+ $self->setGuidOutOfService;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ ok( $endpoint->outOfService eq 1, 'scarlet out of service' );
+
+ $self->resetGuid;
+ $endpoint = Scarlet::GuidEndpoint->new();
+ ok( $endpoint->outOfService eq 0, 'scarlet in service' );
+
+}
+
+sub story38636_http400 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid400;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('400 Bad Request');
+}
+
+sub story38636_http404 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid404;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('404 Not Found');
+}
+
+sub story38636_http500 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid500;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('500 Internal Server Error');
+}
+
+sub story38636_http501 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid501;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('501 Not Implemented');
+}
+
+sub story38636_http502 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid502;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('502 Bad Gateway');
+}
+
+sub story38636_http503 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid503;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('503 Service Unavailable');
+}
+
+sub story38636_http504 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid504;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('504 Gateway Timeout');
+}
+
+sub story38636_http505 : Test(1) {
+ my $self = shift;
+
+ $self->setGuid505;
+ my $endpoint = Scarlet::GuidEndpoint->new();
+ $endpoint->httpGet(999999);
+
+ $self->checkLog('505 HTTP Version Not Supported');
+}
+
+sub checkLog {
+ my $self    = shift;
+ my $pattern = shift;
+
+ file_contains_like( $self->{logFile}, qr{$pattern},
+  'check log - ' . $pattern );
+}
+
+sub deleteLogFile {
+ my $self = shift;
+ unlink( $self->{logFile} ) if ( -e $self->{logFile} );
+}
+
+1;
+
