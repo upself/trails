d58de6a5ecf554472642e6888bb8fad4c692a25b
Tony Zhang <zhysz@cn.ibm.com>
2016-04-01 23:34:41 +0800

    added customizing exception for scheduleF mass load . Story 36170

---
+++ TRAILS/src/main/java/com/ibm/ea/common/NotValidException.java
@@ -0,0 +1,14 @@
+package com.ibm.ea.common;
+
+public class NotValidException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7413356055545153187L;
+	 public NotValidException(String msg)  
+	    {  
+	        super(msg);  
+	    }  
+
+}
