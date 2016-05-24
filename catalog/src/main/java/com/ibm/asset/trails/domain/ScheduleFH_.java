498e8777b1652fe5bbbdb61a751a3eed9a1c238a
Tony Zhang <zhysz@cn.ibm.com>
2016-05-24 17:51:50 +0800

    fixed scheduleF and scheduleFh cascade problem . Story 44978

--- catalog/src/main/java/com/ibm/asset/trails/domain/ScheduleFH_.java
+++ catalog/src/main/java/com/ibm/asset/trails/domain/ScheduleFH_.java
@@ -7,24 +7,24 @@ import javax.persistence.metamodel.StaticMetamodel;
 
 @StaticMetamodel(ScheduleFH.class)
 public class ScheduleFH_ {
-	public static volatile SingularAttribute<ScheduleF, Long> id;
-	public static volatile SingularAttribute<ScheduleF, Long> scheduleFId;
-	public static volatile SingularAttribute<ScheduleF, Long> softwareId;
-	public static volatile SingularAttribute<ScheduleF, String> softwareTitle;
-	public static volatile SingularAttribute<ScheduleF, String> softwareName;
-	public static volatile SingularAttribute<ScheduleF, String> manufacturer;
-	public static volatile SingularAttribute<ScheduleF, String> statusId;
-	public static volatile SingularAttribute<ScheduleF, String> level;
-	public static volatile SingularAttribute<ScheduleF, String> hwOwner;
-	public static volatile SingularAttribute<ScheduleF, String> serial;
-	public static volatile SingularAttribute<ScheduleF, String> machineType;
-	public static volatile SingularAttribute<ScheduleF, String> hostname;
-	public static volatile SingularAttribute<ScheduleF, Long> customerId;
-	public static volatile SingularAttribute<ScheduleF, Long> scopeId;
-	public static volatile SingularAttribute<ScheduleF, Long> sourceId;
-	public static volatile SingularAttribute<ScheduleF, String> SWFinanceResp;
-	public static volatile SingularAttribute<ScheduleF, String> sourceLocation;
-	public static volatile SingularAttribute<ScheduleF, String> remoteUser;
-	public static volatile SingularAttribute<ScheduleF, Date> recordTime;
-	public static volatile SingularAttribute<ScheduleF, String> businessJustification;
+	public static volatile SingularAttribute<ScheduleFH, Long> id;
+	public static volatile SingularAttribute<ScheduleFH, ScheduleF> scheduleF;
+	public static volatile SingularAttribute<ScheduleFH, Long> softwareId;
+	public static volatile SingularAttribute<ScheduleFH, String> softwareTitle;
+	public static volatile SingularAttribute<ScheduleFH, String> softwareName;
+	public static volatile SingularAttribute<ScheduleFH, String> manufacturer;
+	public static volatile SingularAttribute<ScheduleFH, String> statusId;
+	public static volatile SingularAttribute<ScheduleFH, String> level;
+	public static volatile SingularAttribute<ScheduleFH, String> hwOwner;
+	public static volatile SingularAttribute<ScheduleFH, String> serial;
+	public static volatile SingularAttribute<ScheduleFH, String> machineType;
+	public static volatile SingularAttribute<ScheduleFH, String> hostname;
+	public static volatile SingularAttribute<ScheduleFH, Long> customerId;
+	public static volatile SingularAttribute<ScheduleFH, Long> scopeId;
+	public static volatile SingularAttribute<ScheduleFH, Long> sourceId;
+	public static volatile SingularAttribute<ScheduleFH, String> SWFinanceResp;
+	public static volatile SingularAttribute<ScheduleFH, String> sourceLocation;
+	public static volatile SingularAttribute<ScheduleFH, String> remoteUser;
+	public static volatile SingularAttribute<ScheduleFH, Date> recordTime;
+	public static volatile SingularAttribute<ScheduleFH, String> businessJustification;
 }
