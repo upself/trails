351fc61d762c6b35d49ac8e7c41b9abae337bc40
Pavel Novak <Y60704@cz.ibm.com>
2016-07-15 19:30:44 +0200

    Updated variables list, hibernate mapping is missing Story 46614

--- BRAVO/JavaSource/com/ibm/ea/sigbank/InstalledScript.java
+++ BRAVO/JavaSource/com/ibm/ea/sigbank/InstalledScript.java
@@ -2,14 +2,33 @@ package com.ibm.ea.sigbank;
 
 public class InstalledScript {
 
-    private String status;
-
+	private String status;
+    private com.ibm.ea.bravo.software.InstalledSoftware installedSoftware;
+    private com.ibm.ea.sigbank.SoftwareScript softwareScript;
+    private com.ibm.ea.sigbank.BankAccount bankAccount;
 	public String getStatus() {
 		return status;
 	}
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	public com.ibm.ea.bravo.software.InstalledSoftware getInstalledSoftware() {
+		return installedSoftware;
+	}
+	public void setInstalledSoftware(com.ibm.ea.bravo.software.InstalledSoftware installedSoftware) {
+		this.installedSoftware = installedSoftware;
+	}
+	public com.ibm.ea.sigbank.SoftwareScript getSoftwareScript() {
+		return softwareScript;
+	}
+	public void setSoftwareScript(com.ibm.ea.sigbank.SoftwareScript softwareScript) {
+		this.softwareScript = softwareScript;
+	}
+	public com.ibm.ea.sigbank.BankAccount getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(com.ibm.ea.sigbank.BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	} 
 	
 }
--- BRAVO/JavaSource/com/ibm/ea/sigbank/SoftwareScript.java
+++ BRAVO/JavaSource/com/ibm/ea/sigbank/SoftwareScript.java
@@ -2,20 +2,97 @@ package com.ibm.ea.sigbank;
 
 public class SoftwareScript {
 
-    private String softwareName;
+	private String softwareName;
     private String softwareVersion;
-    
+
+    private String mapSoftwareVersion;
+    private String remoteUser;
+    private java.util.Date recordTime;
+    private String status;
+    private String changeJustification;
+    private String catalogType;
+    private String comments;
+
+    private com.ibm.ea.sigbank.Software software;
+
 	public String getSoftwareName() {
 		return softwareName;
 	}
+
 	public void setSoftwareName(String softwareName) {
 		this.softwareName = softwareName;
 	}
+
 	public String getSoftwareVersion() {
 		return softwareVersion;
 	}
+
 	public void setSoftwareVersion(String softwareVersion) {
 		this.softwareVersion = softwareVersion;
-	} 
-	
+	}
+
+	public String getMapSoftwareVersion() {
+		return mapSoftwareVersion;
+	}
+
+	public void setMapSoftwareVersion(String mapSoftwareVersion) {
+		this.mapSoftwareVersion = mapSoftwareVersion;
+	}
+
+	public String getRemoteUser() {
+		return remoteUser;
+	}
+
+	public void setRemoteUser(String remoteUser) {
+		this.remoteUser = remoteUser;
+	}
+
+	public java.util.Date getRecordTime() {
+		return recordTime;
+	}
+
+	public void setRecordTime(java.util.Date recordTime) {
+		this.recordTime = recordTime;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getChangeJustification() {
+		return changeJustification;
+	}
+
+	public void setChangeJustification(String changeJustification) {
+		this.changeJustification = changeJustification;
+	}
+
+	public String getCatalogType() {
+		return catalogType;
+	}
+
+	public void setCatalogType(String catalogType) {
+		this.catalogType = catalogType;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public com.ibm.ea.sigbank.Software getSoftware() {
+		return software;
+	}
+
+	public void setSoftware(com.ibm.ea.sigbank.Software software) {
+		this.software = software;
+	}
+
 }
