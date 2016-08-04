9fb00fd75754b7ff6ff5fcc7b62cffbef093503a
Pavel Novak <Y60704@cz.ibm.com>
2016-08-04 09:44:01 +0200

    Added 5 testHelpers structure, discType test part Story 47335 S12

---
+++ BRAVO/src/test/java/com/ibm/ea/bravo/software/delegatesoftware/test/InstalledSoftwareTestHelper.java
@@ -0,0 +1,32 @@
+package com.ibm.ea.bravo.software.delegatesoftware.test;
+
+import java.util.Date;
+
+import com.ibm.ea.bravo.discrepancy.DiscrepancyType;
+import com.ibm.ea.bravo.software.InstalledSoftware;
+import com.ibm.ea.bravo.software.SoftwareLpar;
+
+import com.ibm.ea.sigbank.ProductInfo;
+
+
+public class InstalledSoftwareTestHelper {
+
+	public static InstalledSoftware createActiveRecord(ProductInfo productInfo, SoftwareLpar softwareLpar, DiscrepancyType discrepancyType) {
+		InstalledSoftware installedSoftware = new InstalledSoftware();
+		installedSoftware.setStatus("ACTIVE");
+		installedSoftware.setRecordTime(new Date());
+		installedSoftware.setRemoteUser("STAGING");
+		installedSoftware.setAuthenticated(0);
+		installedSoftware.setProcessorCount(1);
+		installedSoftware.setDiscrepancyType(discrepancyType);
+		installedSoftware.setSoftwareLpar(softwareLpar);
+		//^^watch out for detached entities - refresh/use one session to build this
+//		installedSoftware.setP;//roductInfo ??
+		return installedSoftware;
+	}
+
+	public static void deleteRecord(Long installedSoftwareId) {
+		// TODO Auto-generated method stub
+		
+	}
+}
