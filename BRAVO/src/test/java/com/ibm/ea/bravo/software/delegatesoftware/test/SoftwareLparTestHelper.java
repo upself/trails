0f34205afb72183eef20e1bafa285c6b11abc441
Pavel Novak <Y60704@cz.ibm.com>
2016-08-05 15:37:42 +0200

    Rename method names Story 47335 S12

--- BRAVO/src/test/java/com/ibm/ea/bravo/software/delegatesoftware/test/SoftwareLparTestHelper.java
+++ BRAVO/src/test/java/com/ibm/ea/bravo/software/delegatesoftware/test/SoftwareLparTestHelper.java
@@ -1,29 +1,50 @@
 package com.ibm.ea.bravo.software.delegatesoftware.test;
 
+
+
 import java.util.Date;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.ibm.ea.bravo.framework.hibernate.HibernateDelegate;
 import com.ibm.ea.bravo.software.SoftwareLpar;
-import com.ibm.ea.cndb.Customer;
 
 
 public class SoftwareLparTestHelper {
 	
-	public static SoftwareLpar createActiveRecord() {
+	public static SoftwareLpar createAsActive() {
 		SoftwareLpar softwareLpar = new SoftwareLpar();
 		softwareLpar.setComputerId("computerId");
 		softwareLpar.setStatus("ACTIVE");
 		softwareLpar.setRecordTime(new Date());
+		softwareLpar.setScantime(new Date());
+		softwareLpar.setAcquisitionTime(new Date());
 		softwareLpar.setRemoteUser("STAGING");
 		softwareLpar.setProcessorCount(1);
 		softwareLpar.setName("softwareLparName");
-		softwareLpar.setCustomer(createTestCustomer());
+		softwareLpar.setCustomer(CustomerTestHelper.getAnyRecord());
+		
+		//Transaction(the rollback, mainly) is probably not needed for one save operation
+		Transaction tx = null;
+		Session session = null;
+		try {
+			session = HibernateDelegate.getSession();
+		    tx = session.beginTransaction();
+		     
+		    session.save(softwareLpar);
+		    
+		    tx.commit();
+		 }
+		 catch (Exception e) {
+		     if (tx != null) tx.rollback();
+		     e.printStackTrace();
+		 }
+		 finally {
+			 session.close();
+		 }
 		return softwareLpar;
-	}
-
-	private static Customer createTestCustomer() {
-		Customer customer = new Customer();
-		customer.setCustomerId(2L);
-		return customer;
+		
 	}
 
 	public static void deleteRecord(Long softwareLparId) {
