5e7b890a4c8ecd0d7f56f10619d1d14b390e94c0
Pavel Novak <Y60704@cz.ibm.com>
2016-10-05 10:37:59 +0200

    Remove bravo project contents

--- BRAVO/WebContent/javascript/misld.js
+++
@@ -1,7 +0,0 @@
-	function checkUncheckAll(theElement) {
-     var theForm = theElement.form, z = 0;
-     while (theForm[z].type == 'checkbox' && theForm[z].name != 'checkall') {
-      theForm[z].checked = theElement.checked;
-      z++;
-     }
-    }
